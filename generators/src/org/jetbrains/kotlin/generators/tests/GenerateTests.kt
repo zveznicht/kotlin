34a59b389c362124be36c635fd7505df960478a0
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2017-06-15 19:04:47 +0300

    Proto: move jvm specific proto comparison tests to separate dir

--- generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -1189,6 +1189,7 @@ fun main(args: Array<String>) {
             model("comparison/packageMembers", extension = null, excludeParentDirs = true)
             model("comparison/unchanged", extension = null, excludeParentDirs = true)
         }
+            model("comparison/jvmOnly", extension = null, excludeParentDirs = true)
     }
 
     testGroup("plugins/plugins-tests/tests",  "plugins/android-extensions/android-extensions-compiler/testData") {
