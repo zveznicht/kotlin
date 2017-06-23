7752d4cb71c3c880f022a78149f0a3e6907889a1
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2017-06-23 06:50:58 +0300

    ~ copy lookup test data

--- generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -1165,11 +1165,10 @@ fun main(args: Array<String>) {
         }
 
         testClass<AbstractJvmLookupTrackerTest> {
-            model("incremental/lookupTracker/common", extension = null, recursive = false)
             model("incremental/lookupTracker/jvm", extension = null, recursive = false)
         }
         testClass<AbstractJsLookupTrackerTest> {
-            model("incremental/lookupTracker/common", extension = null, recursive = false)
+            model("incremental/lookupTracker/js", extension = null, recursive = false)
         }
 
         testClass(AbstractIncrementalLazyCachesTest::class.java) {
