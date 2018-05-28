1b1d28a11643e6a48f719023626812e66b08edea
Dmitry Savvinov <dsavvinov@gmail.com>
2018-05-28 21:18:38 +0300

    Straighforward attempt to fix jumpout data-flow info in elvis

--- compiler/frontend/src/org/jetbrains/kotlin/types/expressions/BasicExpressionTypingVisitor.java
+++ compiler/frontend/src/org/jetbrains/kotlin/types/expressions/BasicExpressionTypingVisitor.java
@@ -1306,7 +1306,7 @@ public class BasicExpressionTypingVisitor extends ExpressionTypingVisitor {
             type = TypeUtils.makeNotNullable(type);
         }
         if (context.contextDependency == DEPENDENT) {
-            return TypeInfoFactoryKt.createTypeInfo(type, dataFlowInfo);
+            return TypeInfoFactoryKt.createTypeInfo(type, dataFlowInfo, loopBreakContinuePossible, context.dataFlowInfo);
         }
 
         // If break or continue was possible, take condition check info as the jump info
