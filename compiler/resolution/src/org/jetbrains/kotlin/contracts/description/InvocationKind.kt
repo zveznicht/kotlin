019e3163183388b3eb310ec289039da9b9aa6dd2
Dmitriy Novozhilov <novodmit.2x@yandex.ru>
2018-09-24 14:14:49 +0300

    Add methods for combine InvocationKind's
    There is two methods added -- `or` and `combine`
    
    `or` is used by CFA for combining two kinds that came from different
    edges of control flow graph
    
    `combine` is analog of `+` operator for invocation kinds

--- compiler/resolution/src/org/jetbrains/kotlin/contracts/description/InvocationKind.kt
+++ compiler/resolution/src/org/jetbrains/kotlin/contracts/description/InvocationKind.kt
@@ -5,12 +5,109 @@
 
 package org.jetbrains.kotlin.contracts.description
 
+/*
+| or   | 0..0 | 0..1 | 1..1 | 1..* | 0..* |
+| ---- | -----| ---- | ---- | ---- | ---- |
+| 0..0 | 0..0 | 0..1 | 0..1 | 0..* | 0..* |
+| 0..1 | 0..1 | 0..1 | 0..1 | 0..* | 0..* |
+| 1..1 | 0..1 | 0..1 | 1..1 | 1..* | 0..* |
+| 1..* | 0..* | 0..* | 1..* | 1..* | 0..* |
+| 0..* | 0..* | 0..* | 0..* | 0..* | 0..* |
+
+| or            | ZERO         | AT_MOST_ONCE | EXACTLY_ONCE  | AT_LEAST_ONCE | UNKNOWN |
+| ------------- | ------------ | ------------ | ------------- | ------------- | ------- |
+| ZERO          | ZERO         | AT_MOST_ONCE | AT_MOST_ONCE  | UNKNOWN       | UNKNOWN |
+| AT_MOST_ONCE  | AT_MOST_ONCE | AT_MOST_ONCE | AT_MOST_ONCE  | UNKNOWN       | UNKNOWN |
+| EXACTLY_ONCE  | AT_MOST_ONCE | AT_MOST_ONCE | EXACTLY_ONCE  | AT_LEAST_ONCE | UNKNOWN |
+| AT_LEAST_ONCE | UNKNOWN      | UNKNOWN      | AT_LEAST_ONCE | AT_LEAST_ONCE | UNKNOWN |
+| UNKNOWN       | UNKNOWN      | UNKNOWN      | UNKNOWN       | UNKNOWN       | UNKNOWN |
+
+
+| combine | 0..0 | 0..1 | 1..1 | 1..* | 0..* |
+| ------- | ---- | ---- | ---- | ---- | ---- |
+| 0..0    | 0..0 | 0..1 | 1..1 | 1..* | 0..* |
+| 0..1    | 0..1 | 0..* | 1..* | 1..* | 0..* |
+| 1..1    | 1..1 | 1..* | 1..* | 1..* | 1..* |
+| 1..*    | 1..* | 1..* | 1..* | 1..* | 1..* |
+| 0..*    | 0..* | 0..* | 1..* | 1..* | 0..* |
+
+| combine       | ZERO          | AT_MOST_ONCE  | EXACTLY_ONCE  | AT_LEAST_ONCE | UNKNOWN       |
+| ------------- | ------------- | ------------- | ------------- | ------------- | ------------- |
+| ZERO          | ZERO          | AT_MOST_ONCE  | EXACTLY_ONCE  | AT_LEAST_ONCE | UNKNOWN       |
+| AT_MOST_ONCE  | AT_MOST_ONCE  | UNKNOWN       | AT_LEAST_ONCE | AT_LEAST_ONCE | UNKNOWN       |
+| EXACTLY_ONCE  | EXACTLY_ONCE  | AT_LEAST_ONCE | AT_LEAST_ONCE | AT_LEAST_ONCE | AT_LEAST_ONCE |
+| AT_LEAST_ONCE | AT_LEAST_ONCE | AT_LEAST_ONCE | AT_LEAST_ONCE | AT_LEAST_ONCE | AT_LEAST_ONCE |
+| UNKNOWN       | UNKNOWN       | UNKNOWN       | AT_LEAST_ONCE | AT_LEAST_ONCE | UNKNOWN       |
+*/
+
 enum class InvocationKind {
-    ZERO,
-    AT_MOST_ONCE,
-    EXACTLY_ONCE,
-    AT_LEAST_ONCE,
-    UNKNOWN
+    ZERO,          // 0..0
+    AT_MOST_ONCE,  // 0..1
+    EXACTLY_ONCE,  // 1..1
+    AT_LEAST_ONCE, // 1..*
+    UNKNOWN;       // 0..*
+
+    companion object {
+        fun or(x: InvocationKind, y: InvocationKind) = when (x) {
+            ZERO -> when (y) {
+                ZERO -> ZERO
+                AT_MOST_ONCE -> AT_MOST_ONCE
+                EXACTLY_ONCE -> AT_MOST_ONCE
+                AT_LEAST_ONCE -> UNKNOWN
+                UNKNOWN -> UNKNOWN
+            }
+            AT_MOST_ONCE -> when (y) {
+                ZERO -> AT_MOST_ONCE
+                AT_MOST_ONCE -> AT_MOST_ONCE
+                EXACTLY_ONCE -> AT_MOST_ONCE
+                AT_LEAST_ONCE -> UNKNOWN
+                UNKNOWN -> UNKNOWN
+            }
+            EXACTLY_ONCE -> when (y) {
+                ZERO -> AT_MOST_ONCE
+                AT_MOST_ONCE -> AT_MOST_ONCE
+                EXACTLY_ONCE -> EXACTLY_ONCE
+                AT_LEAST_ONCE -> AT_LEAST_ONCE
+                UNKNOWN -> UNKNOWN
+            }
+            AT_LEAST_ONCE -> when (y) {
+                ZERO -> UNKNOWN
+                AT_MOST_ONCE -> UNKNOWN
+                EXACTLY_ONCE -> AT_LEAST_ONCE
+                AT_LEAST_ONCE -> AT_LEAST_ONCE
+                UNKNOWN -> UNKNOWN
+            }
+            UNKNOWN -> UNKNOWN
+        }
+
+        fun combine(x: InvocationKind, y: InvocationKind) = when (x) {
+            ZERO -> when (y) {
+                ZERO -> ZERO
+                AT_MOST_ONCE -> AT_MOST_ONCE
+                EXACTLY_ONCE -> EXACTLY_ONCE
+                AT_LEAST_ONCE -> AT_LEAST_ONCE
+                UNKNOWN -> UNKNOWN
+            }
+            AT_MOST_ONCE -> when (y) {
+                ZERO -> AT_MOST_ONCE
+                AT_MOST_ONCE -> UNKNOWN
+                EXACTLY_ONCE -> AT_LEAST_ONCE
+                AT_LEAST_ONCE -> AT_LEAST_ONCE
+                UNKNOWN -> UNKNOWN
+            }
+            EXACTLY_ONCE -> when (y) {
+                ZERO -> EXACTLY_ONCE
+                else -> AT_LEAST_ONCE
+            }
+            AT_LEAST_ONCE -> AT_LEAST_ONCE
+            UNKNOWN -> when (y) {
+                EXACTLY_ONCE -> AT_LEAST_ONCE
+                AT_LEAST_ONCE -> AT_LEAST_ONCE
+                else -> UNKNOWN
+            }
+        }
+    }
+
 }
 
 fun InvocationKind.isDefinitelyVisited(): Boolean = this == InvocationKind.EXACTLY_ONCE || this == InvocationKind.AT_LEAST_ONCE
