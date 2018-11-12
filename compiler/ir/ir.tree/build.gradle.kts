61a513735295ceb43faca90906ea0b4d3b180c5b
Georgy Bronnikov <georgy.bronnikov@jetbrains.com>
2018-11-12 21:03:37 +0300

    Mark java external declarations in IrFunction.origin

--- compiler/ir/ir.tree/build.gradle.kts
+++ compiler/ir/ir.tree/build.gradle.kts
@@ -10,6 +10,8 @@ dependencies {
     compile(intellijDep())
     compile(project(":compiler:util"))
     compile(project(":compiler:frontend"))
+    compile(project(":compiler:frontend.java"))
+
     compileOnly(intellijCoreDep()) { includeJars("intellij-core") }
 }
 
