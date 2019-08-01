081ebd5f30f953f35b8d98ef0015346b3cb0d38d
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2019-08-01 18:32:33 +0300

    ~ tests

---
+++ compiler/incremental-compilation-impl/test/org/jetbrains/kotlin/incremental/AbstractIncrementalJsKlibCompilerRunnerTest.kt
@@ -0,0 +1,15 @@
+package org.jetbrains.kotlin.incremental
+
+import org.jetbrains.kotlin.cli.common.arguments.K2JSCompilerArguments
+import java.io.File
+
+abstract class AbstractIncrementalJsKlibCompilerRunnerTest : AbstractIncrementalJsCompilerRunnerTest() {
+    override fun createCompilerArguments(destinationDir: File, testDir: File): K2JSCompilerArguments =
+        K2JSCompilerArguments().apply {
+            libraries = "/home/user/projects/kotlin/compiler/ir/serialization.js/build/reduced-runtime.klib"
+            outputFile = File(destinationDir, "${testDir.name}.klib").path
+            sourceMap = true
+            irBackend = true
+            irProduceOnly = "klib"
+        }
+}
