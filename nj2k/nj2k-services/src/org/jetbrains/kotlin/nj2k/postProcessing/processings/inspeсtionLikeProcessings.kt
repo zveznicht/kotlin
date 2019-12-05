7097c0ec4c8aa2affec52b6ee91ee54a1ac89297
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2019-12-05 18:07:28 +0300

    EA-210533: fix NPE in LiftReturnOrAssignmentInspection
    It may be still active after its application due to slow IDEA responsiveness
    Do not try to apply it again on invalid PSI elements

--- nj2k/nj2k-services/src/org/jetbrains/kotlin/nj2k/postProcessing/processings/inspeсtionLikeProcessings.kt
+++ nj2k/nj2k-services/src/org/jetbrains/kotlin/nj2k/postProcessing/processings/inspeсtionLikeProcessings.kt
@@ -481,7 +481,7 @@ class LiftAssignmentInspectionBasedProcessing :
         } ?: false
 
     override fun apply(element: KtExpression) {
-        BranchedFoldingUtils.foldToAssignment(element)
+        BranchedFoldingUtils.tryFoldToAssignment(element)
     }
 }
 
