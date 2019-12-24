396730575e3358f488aebe3e08f2ce1590ff299c
Anton Bannykh <anton.bannykh@jetbrains.com>
2019-12-24 21:58:51 +0300

    ThrowableLowering

--- compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/util/IrUtils.kt
+++ compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/util/IrUtils.kt
@@ -618,6 +618,15 @@ val IrDeclaration.fileOrNull: IrFile?
 val IrDeclaration.file: IrFile
     get() = fileOrNull ?: TODO("Unknown file")
 
+val IrDeclaration.classOrNull: IrClass?
+    get() = parent.let {
+        when (it) {
+            is IrClass -> it
+            is IrDeclaration -> it.classOrNull
+            else -> null
+        }
+    }
+
 val IrFunction.allTypeParameters: List<IrTypeParameter>
     get() = if (this is IrConstructor)
         parentAsClass.typeParameters + typeParameters
