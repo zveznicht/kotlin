e559ca21c5aaa05b15aaa8a47552943151b2ff7a
kvirolainen <zaoozka@gmail.com>
2020-01-05 13:18:54 +0300

    Samples on flatten, unzip

--- libraries/stdlib/samples/test/samples/collections/sequences.kt
+++ libraries/stdlib/samples/test/samples/collections/sequences.kt
@@ -242,6 +242,34 @@ class Sequences {
             assertPrints(even, "[0, 2, 8, 34]")
             assertPrints(odd, "[1, 1, 3, 5, 13, 21]")
         }
+
+        @Sample
+        fun flattenSequenceOfSequences() {
+            val sequence = generateSequence(1) { it + 1 }
+            val sequenceOfSequences = sequence.map { num ->
+                generateSequence { num }.take(num)
+            }
+
+            assertPrints(sequenceOfSequences.flatten().take(10).toList(), "[1, 2, 2, 3, 3, 3, 4, 4, 4, 4]")
+        }
+
+        @Sample
+        fun flattenSequenceOfLists() {
+            val sequence = generateSequence(1) { it + 1 }
+            val sequenceOfLists = sequence.map { num ->
+                generateSequence { num }.take(num).toList()
+            }
+
+            assertPrints(sequenceOfLists.flatten().take(10).toList(), "[1, 2, 2, 3, 3, 3, 4, 4, 4, 4]")
+        }
+
+        @Sample
+        fun unzip() {
+            val result = generateSequence(1 to 1) { it.first + 1 to it.second * 2 }.take(10).unzip()
+
+            assertPrints(result.first.toList(), "[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]")
+            assertPrints(result.second.toList(), "[1, 2, 4, 8, 16, 32, 64, 128, 256, 512]")
+        }
     }
 
 }
