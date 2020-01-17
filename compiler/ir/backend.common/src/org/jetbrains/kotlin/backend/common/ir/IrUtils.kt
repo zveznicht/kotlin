4ffceae7ef3c28d51d4c697acfc8a35ab995042f
Anton Bannykh <Anton.Bannykh@jetbrains.com>
2020-01-17 19:07:37 +0300

    Simple stage layers + enforce model

--- compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/ir/IrUtils.kt
+++ compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/ir/IrUtils.kt
@@ -352,7 +352,9 @@ fun IrType.remapTypeParameters(
 
 /* Copied from K/N */
 fun IrDeclarationContainer.addChild(declaration: IrDeclaration) {
-    this.declarations += declaration
+    stageController.unrestrictDeclarationListsAccess {
+        this.declarations += declaration
+    }
     declaration.accept(SetDeclarationsParentVisitor, this)
 }
 
@@ -427,6 +429,7 @@ fun IrClass.simpleFunctions() = declarations.flatMap {
         else -> emptyList()
     }
 }
+
 
 fun IrClass.createParameterDeclarations() {
     assert (thisReceiver == null)
