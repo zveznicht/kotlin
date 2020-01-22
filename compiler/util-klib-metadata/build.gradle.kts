5842d586ecbdbd55486a5861a10a0c9380499d0f
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2020-01-22 13:15:48 +0700

    Add missed dependency on compiler in 'util-klib-metadata'

--- compiler/util-klib-metadata/build.gradle.kts
+++ compiler/util-klib-metadata/build.gradle.kts
@@ -1,15 +1,28 @@
+import org.gradle.api.artifacts.maven.Conf2ScopeMappingContainer.COMPILE
+
 plugins {
+    maven
     kotlin("jvm")
     id("jps-compatible")
+}
+
+val mavenCompileScope by configurations.creating {
+    the<MavenPluginConvention>()
+        .conf2ScopeMappings
+        .addMapping(0, this, COMPILE)
 }
 
 description = "Common klib metadata reader and writer"
 
 dependencies {
+     // Compile-only dependencies are needed for compilation of this module:
     compileOnly(project(":compiler:cli-common"))
     compileOnly(project(":compiler:frontend"))
     compileOnly(project(":core:deserialization"))
     compileOnly(project(":compiler:serialization"))
+
+    // This dependency is necessary to keep the right dependency record inside of POM file:
+    mavenCompileScope(project(":kotlin-compiler"))
 
     compile(kotlinStdlib())
     compile(project(":kotlin-util-io"))
