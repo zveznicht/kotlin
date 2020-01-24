35338b76f05b5eefe11eb978a30558096d6c6793
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2020-01-24 22:15:34 +0300

    *TEMP* Advance bootstrap. To be replaced with right bootstrap version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,12 @@ buildscript {
     extra["defaultSnapshotVersion"] = "1.4-SNAPSHOT"
     val cacheRedirectorEnabled = findProperty("cacheRedirectorEnabled")?.toString()?.toBoolean() == true
 
-    kotlinBootstrapFrom(BootstrapOption.BintrayBootstrap("1.4.0-dev-1075", cacheRedirectorEnabled))
+    kotlinBootstrapFrom(BootstrapOption.TeamCity(
+        "1.4.0-dev-1658",
+        projectExtId = "Kotlin_KotlinDev_Compiler",
+        onlySuccessBootstrap = false,
+        teamcityUrl = "https://buildserver.labs.intellij.net/"
+    ))
 
     repositories {
         bootstrapKotlinRepo?.let(::maven)
