9baa0ce621d9752faf32f71b8d07dcc8a9c73be9
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2020-01-29 19:54:43 +0300

    201: -- some improvements

--- idea/src/org/jetbrains/kotlin/idea/hierarchy/calls/KotlinCallHierarchyNodeDescriptor.kt
+++ idea/src/org/jetbrains/kotlin/idea/hierarchy/calls/KotlinCallHierarchyNodeDescriptor.kt
@@ -29,6 +29,7 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiReference
 import com.intellij.ui.LayeredIcon
 import org.jetbrains.kotlin.descriptors.*
+import org.jetbrains.kotlin.idea.caches.resolve.resolveToDescriptorIfAny
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.renderer.DescriptorRenderer
 import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
@@ -40,12 +41,12 @@ class KotlinCallHierarchyNodeDescriptor(
     element: KtElement,
     isBase: Boolean,
     navigateToReference: Boolean
-) : HierarchyNodeDescriptor(element.project, parentDescriptor, element, isBase),
-    Navigatable {
+) : HierarchyNodeDescriptor(element.project, parentDescriptor, element, isBase), Navigatable {
     private var usageCount = 1
-    private val references: MutableSet<PsiReference> =
-        HashSet()
-    private val javaDelegate: CallHierarchyNodeDescriptor
+    private val references: MutableSet<PsiReference> = HashSet()
+    private val javaDelegate: CallHierarchyNodeDescriptor =
+        CallHierarchyNodeDescriptor(myProject, null, element, isBase, navigateToReference)
+
     fun incrementUsageCount() {
         usageCount++
         javaDelegate.incrementUsageCount()
@@ -64,36 +65,43 @@ class KotlinCallHierarchyNodeDescriptor(
     override fun update(): Boolean {
         val oldText = myHighlightedText
         val oldIcon = icon
-        var flags = Iconable.ICON_FLAG_VISIBILITY
-        if (isMarkReadOnly) {
-            flags = flags or Iconable.ICON_FLAG_READ_STATUS
+        val flags: Int = if (isMarkReadOnly) {
+            Iconable.ICON_FLAG_VISIBILITY or Iconable.ICON_FLAG_READ_STATUS
+        } else {
+            Iconable.ICON_FLAG_VISIBILITY
         }
+
         var changes = super.update()
+
         val targetElement = psiElement
         val elementText = renderElement(targetElement)
         if (elementText == null) {
             val invalidPrefix = IdeBundle.message("node.hierarchy.invalid")
             if (!myHighlightedText.text.startsWith(invalidPrefix)) {
-                myHighlightedText.beginning
-                    .addText(invalidPrefix, getInvalidPrefixAttributes())
+                myHighlightedText.beginning.addText(invalidPrefix, getInvalidPrefixAttributes())
             }
             return true
         }
-        var newIcon = targetElement!!.getIcon(flags)
-        if (changes && myIsBase) {
-            val icon = LayeredIcon(2)
-            icon.setIcon(newIcon, 0)
-            icon.setIcon(AllIcons.Hierarchy.Base, 1, -AllIcons.Hierarchy.Base.iconWidth / 2, 0)
-            newIcon = icon
+
+        val elementIcon = targetElement!!.getIcon(flags)
+        icon = if (changes && myIsBase) {
+            LayeredIcon(2).apply {
+                setIcon(elementIcon, 0)
+                setIcon(AllIcons.Hierarchy.Base, 1, -AllIcons.Hierarchy.Base.iconWidth / 2, 0)
+            }
+        } else {
+            elementIcon
         }
-        icon = newIcon
+
         myHighlightedText = CompositeAppearance()
-        var mainTextAttributes: TextAttributes? = null
-        if (myColor != null) {
-            mainTextAttributes = TextAttributes(myColor, null, null, null, Font.PLAIN)
+
+        val mainTextAttributes = if (myColor != null) {
+            TextAttributes(myColor, null, null, null, Font.PLAIN)
+        } else {
+            null
         }
-        var packageName =
-            KtPsiUtil.getPackageName((targetElement as KtElement?)!!)
+
+        val packageName = KtPsiUtil.getPackageName((targetElement as KtElement?)!!) ?: ""
         myHighlightedText.ending.addText(elementText, mainTextAttributes)
         if (usageCount > 1) {
             myHighlightedText.ending.addText(
@@ -101,16 +109,14 @@ class KotlinCallHierarchyNodeDescriptor(
                 getUsageCountPrefixAttributes()
             )
         }
-        if (packageName == null) {
-            packageName = ""
-        }
-        myHighlightedText.ending
-            .addText("  ($packageName)", getPackageNameAttributes())
+
+        myHighlightedText.ending.addText("  ($packageName)", getPackageNameAttributes())
         myName = myHighlightedText.text
-        if (!(Comparing.equal(myHighlightedText, oldText) && Comparing.equal(icon, oldIcon))
-        ) {
+
+        if (!(Comparing.equal(myHighlightedText, oldText) && Comparing.equal(icon, oldIcon))) {
             changes = true
         }
+
         return changes
     }
 
@@ -134,30 +140,40 @@ class KotlinCallHierarchyNodeDescriptor(
             if (element !is KtNamedDeclaration) {
                 return null
             }
-            var descriptor: DeclarationDescriptor? =
-                element as KtNamedDeclaration?. resolveToDescriptorIfAny BodyResolveMode.PARTIAL
-                    ?: return null
+
+            var descriptor = element.resolveToDescriptorIfAny(BodyResolveMode.PARTIAL) ?: return null
             val elementText: String?
-            if (element is KtClassOrObject) {
-                if (element is KtObjectDeclaration && element.isCompanion()) {
-                    descriptor = descriptor.containingDeclaration
-                    if (descriptor !is ClassDescriptor) return null
-                    elementText = renderClassOrObject(descriptor)
-                } else if (element is KtEnumEntry) {
-                    elementText = element.name
-                } else {
-                    elementText = if (element.name != null) {
-                        renderClassOrObject(descriptor as ClassDescriptor)
-                    } else {
-                        "[anonymous]"
+            when (element) {
+                is KtClassOrObject -> {
+                    when {
+                        element is KtObjectDeclaration && element.isCompanion() -> {
+                            val containingDescriptor = descriptor.containingDeclaration
+                            if (containingDescriptor !is ClassDescriptor) return null
+                            descriptor = containingDescriptor
+                            elementText = renderClassOrObject(descriptor)
+                        }
+                        element is KtEnumEntry -> {
+                            elementText = element.name
+                        }
+                        else -> {
+                            elementText = if (element.name != null) {
+                                renderClassOrObject(descriptor as ClassDescriptor)
+                            } else {
+                                "[anonymous]"
+                            }
+                        }
                     }
                 }
-            } else if (element is KtNamedFunction || element is KtConstructor<*>) {
-                if (descriptor !is FunctionDescriptor) return null
-                elementText = renderNamedFunction(descriptor)
-            } else if (element is KtProperty) {
-                elementText = element.name
-            } else return null
+                is KtNamedFunction, is KtConstructor<*> -> {
+                    if (descriptor !is FunctionDescriptor) return null
+                    elementText = renderNamedFunction(descriptor)
+                }
+                is KtProperty -> {
+                    elementText = element.name
+                }
+                else -> return null
+            }
+
             if (elementText == null) return null
             var containerText: String? = null
             var containerDescriptor = descriptor.containingDeclaration
@@ -195,9 +211,5 @@ class KotlinCallHierarchyNodeDescriptor(
         private fun renderClassOrObject(descriptor: ClassDescriptor): String {
             return descriptor.name.asString()
         }
-    }
-
-    init {
-        javaDelegate = CallHierarchyNodeDescriptor(myProject, null, element, isBase, navigateToReference)
     }
 }
