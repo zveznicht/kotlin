df64b557d26a703148f20faa39b2f2e5136a6002
Vladimir Ilmov <vladimir.ilmov@jetbrains.com>
2020-01-30 12:49:39 +0100

    [COROUTINE] NPE exception fix if no DebugMetadata in target jvm exists

--- idea/jvm-debugger/jvm-debugger-coroutine/src/org/jetbrains/kotlin/idea/debugger/coroutine/proxy/AsyncStackTraceContext.kt
+++ idea/jvm-debugger/jvm-debugger-coroutine/src/org/jetbrains/kotlin/idea/debugger/coroutine/proxy/AsyncStackTraceContext.kt
@@ -25,9 +25,6 @@ class AsyncStackTraceContext(
     val context: ExecutionContext,
     val method: Method) {
     val log by logger
-    val debugMetadataKtType = context.findClassSafe(
-        DEBUG_METADATA_KT
-    )!!
 
     private companion object {
         const val DEBUG_METADATA_KT = "kotlin.coroutines.jvm.internal.DebugMetadataKt"
@@ -74,20 +71,33 @@ class AsyncStackTraceContext(
         val continuationType = continuation.referenceType() as? ClassType ?: return
         val baseContinuationSupertype = findBaseContinuationSuperSupertype(continuationType) ?: return
 
-        val location = createLocation(continuation)
+        val debugMetadataKtType = debugMetadataKtType()
+        if (debugMetadataKtType is ClassType) {
+            val location = createLocation(continuation, debugMetadataKtType)
 
-        location?.let {
-            val spilledVariables = getSpilledVariables(continuation) ?: mutableListOf()
-            consumer.add(CoroutineStackFrameItem(location, spilledVariables))
+            location?.let {
+                val spilledVariables = getSpilledVariables(continuation, debugMetadataKtType) ?: emptyList()
+                consumer.add(CoroutineStackFrameItem(location, spilledVariables.toTypedArray().toMutableList()))
+            }
+
+            val completionField = baseContinuationSupertype.fieldByName("completion") ?: return
+            val completion = continuation.getValue(completionField) as? ObjectReference ?: return
+            collectFramesRecursively(completion, consumer)
         }
-
-        val completionField = baseContinuationSupertype.fieldByName("completion") ?: return
-        val completion = continuation.getValue(completionField) as? ObjectReference ?: return
-        collectFramesRecursively(completion, consumer)
     }
 
-    private fun createLocation(continuation: ObjectReference) : GeneratedLocation? {
-        val instance = invokeGetStackTraceElement(continuation) ?: return null
+    fun debugMetadataKtType(): ClassType? {
+        val debugMetadataKtType = context.findClassSafe(DEBUG_METADATA_KT)
+        if (debugMetadataKtType != null) {
+            return debugMetadataKtType
+        } else {
+            log.warn("Continuation information found but no $DEBUG_METADATA_KT class exists. Please check kotlin-stdlib version.")
+        }
+        return null
+    }
+
+    private fun createLocation(continuation: ObjectReference, debugMetadataKtType: ClassType): GeneratedLocation? {
+        val instance = invokeGetStackTraceElement(continuation, debugMetadataKtType) ?: return null
         val className = context.invokeMethodAsString(instance, "getClassName") ?: return null
         val methodName = context.invokeMethodAsString(instance, "getMethodName") ?: return null
         val lineNumber = context.invokeMethodAsInt(instance,"getLineNumber")?.takeIf {
@@ -97,18 +107,24 @@ class AsyncStackTraceContext(
         return GeneratedLocation(context.debugProcess, locationClass, methodName, lineNumber)
     }
 
-    private fun invokeGetStackTraceElement(continuation: ObjectReference): ObjectReference? {
+    private fun invokeGetStackTraceElement(continuation: ObjectReference, debugMetadataKtType: ClassType): ObjectReference? {
         val stackTraceElement =
             context.invokeMethodAsObject(debugMetadataKtType, "getStackTraceElement", continuation) ?: return null
-        // redundant i believe
+
         stackTraceElement.referenceType().takeIf { it.name() == StackTraceElement::class.java.name } ?: return null
 
         context.keepReference(stackTraceElement)
         return stackTraceElement
     }
 
-    fun getSpilledVariables(continuation: ObjectReference): MutableList<XNamedValue>? {
+    fun getSpilledVariables(continuation: ObjectReference): List<XNamedValue>? {
+        debugMetadataKtType()?.let {
+            return getSpilledVariables(continuation, it)
+        }
+        return null
+    }
 
+    fun getSpilledVariables(continuation: ObjectReference, debugMetadataKtType: ClassType): List<XNamedValue>? {
         val rawSpilledVariables =
             context.invokeMethodAsArray(debugMetadataKtType, "getSpilledVariableFieldMapping", "(Lkotlin/coroutines/jvm/internal/BaseContinuationImpl;)[Ljava/lang/String;", continuation) ?: return null
 
