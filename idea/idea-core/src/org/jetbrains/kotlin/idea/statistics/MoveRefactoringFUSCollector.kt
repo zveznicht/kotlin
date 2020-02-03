fbbacc2e1661d79d45fcc1826fe963a8eb713bf7
Anton Yalyshev <anton.yalyshev@jetbrains.com>
2020-02-03 09:37:02 +0300

    Add FUS Collector for Move Refactoring

---
+++ idea/idea-core/src/org/jetbrains/kotlin/idea/statistics/MoveRefactoringFUSCollector.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.idea.statistics
+
+object MoveRefactoringFUSCollector {
+
+    /**
+     * @param isDefault is something changed in Move Refactoring Dialog check-boxes state
+     */
+    fun log(
+        timeStarted: Long,
+        timeFinished: Long,
+        numberOfFiles: Int,
+        destination: MoveRefactoringDestination,
+        isDefault: Boolean
+    ) {
+
+        val data = mapOf(
+            "lagging" to (timeFinished - timeStarted).toString(),
+            "destination" to destination.toString(),
+            "number_of_files" to numberOfFiles.toString(),
+            "are_settings_changed" to isDefault.toString()
+        )
+
+        KotlinFUSLogger.log(FUSEventGroups.Refactoring, "Move", data)
+    }
+
+    enum class MoveRefactoringDestination {
+        PACKAGE, FILE
+    }
+}
