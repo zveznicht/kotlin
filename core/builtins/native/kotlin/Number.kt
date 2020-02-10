a3edbc75395067e6987fdf5a332622f7028ea039
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2020-02-10 23:21:53 +0300

    Mark Number class with CompileTimeCalculation annotation

--- core/builtins/native/kotlin/Number.kt
+++ core/builtins/native/kotlin/Number.kt
@@ -19,6 +19,7 @@ package kotlin
 /**
  * Superclass for all platform classes representing numeric values.
  */
+@CompileTimeCalculation
 public abstract class Number {
     /**
      * Returns the value of this number as a [Double], which may involve rounding.
