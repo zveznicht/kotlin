3cb3a870325feb838e64ed0818ac4ddcb9876d0a
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2020-02-21 15:56:32 +0300

    Always check project.useCompositeAnalysis in findAnalyzerServices
    The important things here are changes in
    ResovlerElementCache/PerFileAnalysisCache. Previously, we'd always use
    'CompositeAnalyzerServices' for common-platform, even with HMPP turned
    off.
    
    However, with HMPP turned off, common-platform is always coerced to
    [JVM, JS, Native], no matter which platforms that project actually
    targets.
    So, even if project compiles only for JVM/Native, we'll detect
    platform of common module as [JVM, JS, Native], build composite analyzer
    services *which include all checkers from all platforms*, and then
    report false-positive errors from JS checkers.
    
    ^KT-35031 Fixed
    ^KT-33573 Fixed
    ^KT-34925 Fixed

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/project/analyzerServices.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/project/analyzerServices.kt
@@ -5,6 +5,8 @@
 
 package org.jetbrains.kotlin.idea.project
 
+import com.intellij.openapi.project.Project
+import org.jetbrains.kotlin.analyzer.common.CommonPlatformAnalyzerServices
 import org.jetbrains.kotlin.caches.resolve.CompositeAnalyzerServices
 import org.jetbrains.kotlin.js.resolve.JsPlatformAnalyzerServices
 import org.jetbrains.kotlin.platform.SimplePlatform
@@ -18,12 +20,11 @@ import org.jetbrains.kotlin.resolve.jvm.platform.JvmPlatformAnalyzerServices
 import org.jetbrains.kotlin.resolve.konan.platform.NativePlatformAnalyzerServices
 import java.lang.IllegalStateException
 
-val TargetPlatform.findAnalyzerServices: PlatformDependentAnalyzerServices
-    get() =
-        when {
-            isCommon() -> CompositeAnalyzerServices(this.componentPlatforms.map { it.findAnalyzerServices })
-            else -> single().findAnalyzerServices
-        }
+fun TargetPlatform.findAnalyzerServices(project: Project): PlatformDependentAnalyzerServices = when {
+    isCommon() && project.useCompositeAnalysis -> CompositeAnalyzerServices(componentPlatforms.map { it.findAnalyzerServices })
+    isCommon() && !project.useCompositeAnalysis -> CommonPlatformAnalyzerServices
+    else -> single().findAnalyzerServices
+}
 
 val SimplePlatform.findAnalyzerServices: PlatformDependentAnalyzerServices
     get() = when (this) {
