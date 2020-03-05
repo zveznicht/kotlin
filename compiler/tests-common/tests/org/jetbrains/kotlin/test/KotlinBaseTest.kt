eebad9769a799de2467d763d7fbeee0aa9164e2d
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2020-03-05 14:18:41 +0100

    Support per files test directives

--- compiler/tests-common/tests/org/jetbrains/kotlin/test/KotlinBaseTest.kt
+++ compiler/tests-common/tests/org/jetbrains/kotlin/test/KotlinBaseTest.kt
@@ -70,7 +70,11 @@ abstract class KotlinBaseTest<F : KotlinBaseTest.TestFile> : KtUsefulTestCase() 
     }
 
 
-    open class TestFile(@JvmField val name: String, @JvmField val content: String) : Comparable<TestFile> {
+    open class TestFile @JvmOverloads constructor(
+        @JvmField val name: String,
+        @JvmField val content: String,
+        @JvmField val directives: Map<String, String?> = emptyMap()
+    ) : Comparable<TestFile> {
         override operator fun compareTo(other: TestFile): Int {
             return name.compareTo(other.name)
         }
