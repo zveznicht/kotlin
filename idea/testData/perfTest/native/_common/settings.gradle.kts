f6d88aa2ce488d31865a16268abf039cd5fc010b
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2020-03-23 16:20:05 +0700

    TEMP: Run K/N IDE perf test on 1.4-SNAPSHOT (don't merge to master)

--- idea/testData/perfTest/native/_common/settings.gradle.kts
+++ idea/testData/perfTest/native/_common/settings.gradle.kts
@@ -9,6 +9,7 @@ pluginManagement {
     }
 
     repositories {
+        mavenLocal()
         gradlePluginPortal()
         maven("https://dl.bintray.com/kotlin/kotlin-dev")
         maven("https://dl.bintray.com/kotlin/kotlin-eap")
