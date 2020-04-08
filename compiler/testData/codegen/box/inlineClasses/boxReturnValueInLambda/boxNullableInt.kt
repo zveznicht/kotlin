59b6d1600f96765d522fe585fdb0cdbe39caec80
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2020-04-08 12:28:17 +0300

    Box inline class return value in lambdas (including suspend lambdas)

---
+++ compiler/testData/codegen/box/inlineClasses/boxReturnValueInLambda/boxNullableInt.kt
@@ -0,0 +1,10 @@
+// IGNORE_BACKEND_FIR: JVM_IR
+// IGNORE_BACKEND: JVM_IR
+
+inline class X(val x: Int?)
+
+fun useX(x: X): String = if (x.x == 42) "OK" else "fail: $x"
+
+fun <T> call(fn: () -> T) = fn()
+
+fun box() = useX(call { X(42) })
