4070f6b4e261c53dcfd2f7862180cd166763fb54
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2020-05-15 16:46:55 +0300

    -- prohibit @JvnName on functions mangled because of return type

--- compiler/testData/diagnostics/testsWithStdLib/annotations/annotationApplicability/jvmNameOnMangledNames.fir.kt
+++ compiler/testData/diagnostics/testsWithStdLib/annotations/annotationApplicability/jvmNameOnMangledNames.fir.kt
@@ -2,12 +2,26 @@
 // !DIAGNOSTICS: -UNUSED_PARAMETER
 
 inline class Foo(val x: Int) {
-    @JvmName("other")
+    <!INAPPLICABLE_JVM_NAME!>@JvmName("other")<!>
     fun simple() {}
 }
 
-@JvmName("bad")
+<!INAPPLICABLE_JVM_NAME!>@JvmName("bad")<!>
 fun bar(f: Foo) {}
 
 @JvmName("good")
 fun baz(r: Result<Int>) {}
+
+@JvmName("test")
+fun returnsInlineClass() = Foo(1)
+
+@JvmName("test")
+fun returnsKotlinResult(a: Result<Int>): Result<Int> = a
+
+class C {
+    <!INAPPLICABLE_JVM_NAME!>@JvmName("test")<!>
+    fun returnsInlineClass() = Foo(1)
+
+    <!INAPPLICABLE_JVM_NAME!>@JvmName("test")<!>
+    fun returnsKotlinResult(a: Result<Int>): Result<Int> = a
+}
