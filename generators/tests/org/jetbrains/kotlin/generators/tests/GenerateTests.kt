7e51865095c2965603faa38e55785c184b9d82a3
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2020-05-18 11:37:09 +0300

    FIR IDE: Begin implementing semantic highlighting via FIR
    * Introduce frontend api module & implement api for FIR
    * Implement some basic declaration highlighting for FIR

--- generators/tests/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/tests/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -907,6 +907,10 @@ fun main() {
     testGroup("idea/idea-fir/tests", "idea/testData") {
         testClass<AbstractFirMultiModuleResolveTest> {
             model("fir/multiModule", recursive = false, extension = null)
+        }
+
+        testClass<AbstractFirHighlightingTest> {
+            model("highlighter")
         }
 
         testClass<AbstractFirLazyResolveTest> {
