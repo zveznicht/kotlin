5ba69b5563a71769848d5fe95ebeaae9a8a3dec9
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2020-05-19 19:11:53 +0200

    Fix merging two sources with same type

--- compiler/backend/src/org/jetbrains/kotlin/codegen/coroutines/RedundantLocalsEliminationMethodTransformer.kt
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/coroutines/RedundantLocalsEliminationMethodTransformer.kt
@@ -5,50 +5,60 @@
 
 package org.jetbrains.kotlin.codegen.coroutines
 
-import org.jetbrains.annotations.TestOnly
-import org.jetbrains.kotlin.codegen.inline.insnOpcodeText
 import org.jetbrains.kotlin.codegen.inline.nodeText
 import org.jetbrains.kotlin.codegen.optimization.boxing.isUnitInstance
-import org.jetbrains.kotlin.codegen.optimization.common.InsnSequence
 import org.jetbrains.kotlin.codegen.optimization.common.MethodAnalyzer
 import org.jetbrains.kotlin.codegen.optimization.common.asSequence
 import org.jetbrains.kotlin.codegen.optimization.common.removeAll
 import org.jetbrains.kotlin.codegen.optimization.fixStack.top
 import org.jetbrains.kotlin.codegen.optimization.transformer.MethodTransformer
-import org.jetbrains.kotlin.config.LanguageVersionSettings
 import org.jetbrains.kotlin.resolve.jvm.AsmTypes
 import org.jetbrains.org.objectweb.asm.Opcodes
 import org.jetbrains.org.objectweb.asm.Type
 import org.jetbrains.org.objectweb.asm.tree.*
 import org.jetbrains.org.objectweb.asm.tree.analysis.BasicInterpreter
 import org.jetbrains.org.objectweb.asm.tree.analysis.BasicValue
-import java.io.File
-import java.time.Instant
-import java.time.Instant.now
-import java.time.LocalDateTime
-import java.util.*
+import org.jetbrains.org.objectweb.asm.tree.analysis.Frame
 
 private class PossibleSpilledValue(val source: AbstractInsnNode, type: Type?) : BasicValue(type) {
     val usages = mutableSetOf<AbstractInsnNode>()
 
-    override fun toString(): String = source.insnOpcodeText
+    override fun toString(): String = when {
+        source.opcode == Opcodes.ALOAD -> "" + (source as VarInsnNode).`var`
+        source.isUnitInstance() -> "U"
+        else -> error("unreachable")
+    }
+
+    override fun equals(other: Any?): Boolean =
+        other is PossibleSpilledValue && source == other.source
+
+    override fun hashCode(): Int = super.hashCode() xor source.hashCode()
 }
 
-private object ConstructedValue : BasicValue(AsmTypes.OBJECT_TYPE)
+private object NonSpillableValue : BasicValue(AsmTypes.OBJECT_TYPE) {
+    override fun equals(other: Any?): Boolean = other is NonSpillableValue
 
-private class RedundantSpillingInterpreter(
-    private val languageVersionSettings: LanguageVersionSettings,
-    private val methodNode: MethodNode
-) :
-    BasicInterpreter(Opcodes.API_VERSION) {
+    override fun toString(): String = "N"
+}
+
+private object ConstructedValue : BasicValue(AsmTypes.OBJECT_TYPE) {
+    override fun equals(other: Any?): Boolean = other is ConstructedValue
+
+    override fun toString(): String = "C"
+}
+
+fun BasicValue?.nonspillable(): BasicValue? = if (this?.type?.sort == Type.OBJECT) NonSpillableValue else this
+
+private class RedundantSpillingInterpreter : BasicInterpreter(Opcodes.API_VERSION) {
     val possibleSpilledValues = mutableSetOf<PossibleSpilledValue>()
 
     override fun newOperation(insn: AbstractInsnNode): BasicValue? {
         if (insn.opcode == Opcodes.NEW) return ConstructedValue
         val basicValue = super.newOperation(insn)
-        return if (insn.isUnitInstance())
-            PossibleSpilledValue(insn, basicValue.type).also { possibleSpilledValues += it }
-        else basicValue
+        return when {
+            insn.isUnitInstance() -> PossibleSpilledValue(insn, basicValue.type).also { possibleSpilledValues += it }
+            else -> basicValue.nonspillable()
+        }
     }
 
     override fun copyOperation(insn: AbstractInsnNode, value: BasicValue?): BasicValue? =
@@ -57,34 +67,31 @@ private class RedundantSpillingInterpreter(
             value is PossibleSpilledValue -> {
                 value.usages += insn
                 if (insn.opcode == Opcodes.ALOAD || insn.opcode == Opcodes.ASTORE) value
-                else super.newValue(value.type)
+                else value.nonspillable()
             }
             insn.opcode == Opcodes.ALOAD -> PossibleSpilledValue(insn, AsmTypes.OBJECT_TYPE)
-            else ->
-                super.newValue(value?.type)
+            else -> value?.nonspillable()
         }
 
     override fun naryOperation(insn: AbstractInsnNode, values: MutableList<out BasicValue?>): BasicValue? {
         for (value in values.filterIsInstance<PossibleSpilledValue>()) {
             value.usages += insn
         }
-        return super.naryOperation(insn, values)
+        return super.naryOperation(insn, values)?.nonspillable()
     }
 
     override fun merge(v: BasicValue?, w: BasicValue?): BasicValue? =
-        if (v is PossibleSpilledValue && w is PossibleSpilledValue && v.source == w.source) v else super.newValue(v?.type)
+        if (v is PossibleSpilledValue && w is PossibleSpilledValue && v.source == w.source) v
+        else v?.nonspillable()
 }
 
 // Inliner emits a lot of locals during inlining.
 // Remove all of them since these locals are
 //  1) going to be spilled into continuation object
 //  2) breaking tail-call elimination
-internal class RedundantLocalsEliminationMethodTransformer(
-    private val languageVersionSettings: LanguageVersionSettings,
-    private val suspensionPoints: List<SuspensionPoint>
-) : MethodTransformer() {
+internal class RedundantLocalsEliminationMethodTransformer(private val suspensionPoints: List<SuspensionPoint>) : MethodTransformer() {
     override fun transform(internalClassName: String, methodNode: MethodNode) {
-        val interpreter = RedundantSpillingInterpreter(languageVersionSettings, methodNode)
+        val interpreter = RedundantSpillingInterpreter()
         val frames = MethodAnalyzer<BasicValue>(internalClassName, methodNode, interpreter).analyze()
 
         val toDelete = mutableSetOf<AbstractInsnNode>()
@@ -122,4 +129,38 @@ internal class RedundantLocalsEliminationMethodTransformer(
         is TypeInsnNode -> TypeInsnNode(opcode, desc)
         else -> error("clone of $this is not implemented yet")
     }
+}
+
+// Handy debugging routing
+fun MethodNode.nodeTextWithFrames(frames: Array<Frame<BasicValue>?>): String {
+    var insns = nodeText.split("\n")
+    val first = insns.indexOfLast { it.trim().startsWith("@") } + 1
+    var last = insns.indexOfFirst { it.trim().startsWith("LOCALVARIABLE") }
+    if (last < 0) last = insns.size
+    val prefix = insns.subList(0, first).joinToString(separator = "\n")
+    val postfix = insns.subList(last, insns.size).joinToString(separator = "\n")
+    insns = insns.subList(first, last)
+    if (insns.any { it.contains("TABLESWITCH")}) {
+        var insideTableSwitch = false
+        var buffer = ""
+        val res = arrayListOf<String>()
+        for (insn in insns) {
+            if (insn.contains("TABLESWITCH")) {
+                insideTableSwitch = true
+            }
+            if (insideTableSwitch) {
+                buffer += insn
+                if (insn.contains("default")) {
+                    insideTableSwitch = false
+                    res += buffer
+                    buffer = ""
+                    continue
+                }
+            } else {
+                res += insn
+            }
+        }
+        insns = res
+    }
+    return prefix + "\n" + insns.withIndex().joinToString(separator = "\n") { (index, insn) -> "${frames[index]}\t$insn" } + "\n" + postfix
 }
