817bd6312b897c6b9ad0bff2e23f59790e71ec9f
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2020-05-19 12:35:41 +0300

    -- bs against 1.4.0-dev-8702

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ import proguard.gradle.ProGuardTask
 buildscript {
     val cacheRedirectorEnabled = findProperty("cacheRedirectorEnabled")?.toString()?.toBoolean() == true
 
-    kotlinBootstrapFrom(BootstrapOption.BintrayBootstrap(kotlinBuildProperties.kotlinBootstrapVersion!!, cacheRedirectorEnabled))
-//    kotlinBootstrapFrom(BootstrapOption.BintrayBootstrap(kotlinBuildProperties.teamCityBootstrapVersion!!, cacheRedirectorEnabled))
+//    kotlinBootstrapFrom(BootstrapOption.BintrayBootstrap(kotlinBuildProperties.kotlinBootstrapVersion!!, cacheRedirectorEnabled))
+    kotlinBootstrapFrom(BootstrapOption.BintrayBootstrap(kotlinBuildProperties.teamCityBootstrapVersion!!, cacheRedirectorEnabled))
 //    kotlinBootstrapFrom(BootstrapOption.Local(localPath = "C:\\WORK\\kotlin-gradle\\build\\repo "))
 
     repositories {
