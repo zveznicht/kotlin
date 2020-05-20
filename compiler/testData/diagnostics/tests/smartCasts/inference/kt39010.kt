cbdbd11deab0d969b39bbe43510f83130e8c5b58
Pavel Kirpichenkov <pavel.kirpichenkov@jetbrains.com>
2020-05-20 12:49:38 +0300

    [NI] Report unstable smart cast directly instead of using SmartCastManager
    ^KT-39010

---
+++ compiler/testData/diagnostics/tests/smartCasts/inference/kt39010.kt
@@ -0,0 +1,11 @@
+class A<E> {
+    fun foo(): E = TODO()
+}
+
+class B(var a: A<*>?) {
+    fun bar() {
+        if (a != null) {
+            <!SMARTCAST_IMPOSSIBLE!>a<!>.foo() // Ok with NI, not allowed with old inference because smartcast is to a mutable property
+        }
+    }
+}
