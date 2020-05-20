cef677d9095956fa4e907205ba0b002e76914e73
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2020-05-20 21:48:30 +0300

    FIR IDE: Implement FIR reference resolve for some kinds of references
    * implement for:
       * KtForLoopInReference
       * KtInvokeFunctionReference
       * KtPropertyDelegationMethodsReference
       * KtDestructuringDeclarationReference

--- idea/idea-frontend-fir/src/org/jetbrains/kotlin/idea/fir/FirResolutionApi.kt
+++ idea/idea-frontend-fir/src/org/jetbrains/kotlin/idea/fir/FirResolutionApi.kt
@@ -224,6 +224,11 @@ private class FirDesignatedBodyResolveTransformerForIDE(
     }
 }
 
+inline fun <reified E : FirElement> KtElement.getOrBuildFirSafe(
+    state: FirModuleResolveState,
+    phase: FirResolvePhase = FirResolvePhase.BODY_RESOLVE
+) = getOrBuildFir(state, phase) as? E
+
 fun KtElement.getOrBuildFir(
     state: FirModuleResolveState,
     phase: FirResolvePhase = FirResolvePhase.BODY_RESOLVE
