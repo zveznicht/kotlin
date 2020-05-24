da477eca365ba9708952141aa2780ee15ecde45e
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2020-05-24 14:24:52 +0300

    FIR IDE: Implement search of psi elements for deserialized Kotlin declarations
    Fix "FIR IDE: Implement search of psi elements for deserialized Kotlin declarations"

--- compiler/fir/java/src/org/jetbrains/kotlin/fir/java/FirJavaModuleBasedSession.kt
+++ compiler/fir/java/src/org/jetbrains/kotlin/fir/java/FirJavaModuleBasedSession.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2018 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -85,7 +85,7 @@ class FirJavaModuleBasedSession(
 class FirLibrarySession private constructor(
     moduleInfo: ModuleInfo,
     sessionProvider: FirProjectSessionProvider,
-    scope: GlobalSearchScope,
+    val scope: GlobalSearchScope,
     packagePartProvider: PackagePartProvider,
     kotlinClassFinder: KotlinClassFinder,
     javaClassFinder: JavaClassFinder
