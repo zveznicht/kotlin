e94a9bc380e47e5aa16a56e72fff9ec8a2a13f4d
Georgy Bronnikov <georgy.bronnikov@jetbrains.com>
2020-05-26 16:18:23 +0300

    IR: equality checker for symbols that works with FIR-generated IR

--- compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/symbols/IrClassifierEqualityChecker.kt
+++ compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/symbols/IrClassifierEqualityChecker.kt
@@ -5,8 +5,12 @@
 
 package org.jetbrains.kotlin.ir.symbols
 
+import org.jetbrains.kotlin.ir.declarations.IrClass
+import org.jetbrains.kotlin.ir.declarations.IrSymbolOwner
 import org.jetbrains.kotlin.ir.ObsoleteDescriptorBasedAPI
 import org.jetbrains.kotlin.ir.symbols.impl.IrPublicSymbolBase
+import org.jetbrains.kotlin.ir.util.fqNameWhenAvailable
+import org.jetbrains.kotlin.ir.util.isLocalClass
 
 interface IrClassifierEqualityChecker {
     fun areEqual(left: IrClassifierSymbol, right: IrClassifierSymbol): Boolean
@@ -18,10 +22,10 @@ interface IrClassifierEqualityChecker {
 object SignatureEqualityChecker : IrClassifierEqualityChecker {
     override fun areEqual(left: IrClassifierSymbol, right: IrClassifierSymbol): Boolean {
         if (left === right) return true
-        if (left !is IrPublicSymbolBase<*> || right !is IrPublicSymbolBase<*>) {
-            return false
-        }
-        return left.signature == right.signature
+        if (left is IrPublicSymbolBase<*> && right is IrPublicSymbolBase<*> && left.signature == right.signature) return true
+        // Declarations from FIR can have private symbols even when public.
+        if (left.isBound && right.isBound) return checkViaDeclarations(left.owner, right.owner)
+        return false
     }
 
     override fun getHashCode(symbol: IrClassifierSymbol): Int {
@@ -30,4 +34,16 @@ object SignatureEqualityChecker : IrClassifierEqualityChecker {
         }
         return symbol.signature.hashCode()
     }
+
+    private fun checkViaDeclarations(c1: IrSymbolOwner, c2: IrSymbolOwner): Boolean {
+        if (c1 is IrClass && c2 is IrClass) {
+            if (c1.isLocalClass() || c2.isLocalClass())
+                return c1 === c2 // Local declarations should be identical
+
+            return c1.fqNameWhenAvailable == c2.fqNameWhenAvailable
+        }
+
+        return c1 == c2
+    }
+
 }
