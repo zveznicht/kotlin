c930d3c2ad9d5855ccfe2e89d0083959abb0d342
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2020-05-28 17:45:33 +0200

    fixup! Fix bugs with capturing rhs into EXACTLY_ONCE lambda

--- compiler/testData/codegen/box/contracts/forLoop.kt
+++ compiler/testData/codegen/box/contracts/forLoop.kt
@@ -11,26 +11,50 @@ fun runOnce(action: () -> Unit) {
     action()
 }
 
-fun o(): String {
+fun test1(): String {
     var res = ""
-    for (o in listOf("O")) {
+    for (s in listOf("OK")) {
         runOnce {
-            res += o
+            res += s
         }
     }
     return res
 }
 
-fun k(): String {
+fun test2(): String {
     var res = ""
-    for ((k, _) in listOf("K" to "FAIL")) {
+    for (s: String in listOf("OK")) {
         runOnce {
-            res += k
+            res += s
+        }
+    }
+    return res
+}
+
+fun test3(): String {
+    var res = ""
+    for ((s, _) in listOf("OK" to "FAIL")) {
+        runOnce {
+            res += s
+        }
+    }
+    return res
+}
+
+fun test4(): String {
+    var res = ""
+    for ((s: String, _) in listOf("OK" to "FAIL")) {
+        runOnce {
+            res += s
         }
     }
     return res
 }
 
 fun box(): String {
-    return o() + k()
+    test1().let { if (it != "OK") return "FAIL 1: $it" }
+    test2().let { if (it != "OK") return "FAIL 2: $it" }
+    test3().let { if (it != "OK") return "FAIL 3: $it" }
+    test4().let { if (it != "OK") return "FAIL 4: $it" }
+    return "OK"
 }
