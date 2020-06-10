64a67c7eb9349d20e4d2a843f64d9cf064468d9f
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2020-06-10 23:38:14 +0300

    Add tests to check interpretation inside object declaration

--- compiler/testData/ir/interpreter/object.kt
+++ compiler/testData/ir/interpreter/object.kt
@@ -2,13 +2,13 @@
 
 @CompileTimeCalculation
 class A {
-    val a = 10
+    val a = { 10 }()
 
     companion object {
         @CompileTimeCalculation
-        val static = -10
+        val static = { -10 }()
 
-        val willNotCompute = -10
+        val willNotCompute = { -10 }()
 
         @CompileTimeCalculation
         fun getStaticNumber(): Int {
@@ -22,6 +22,14 @@ object Obj {
     val static = "Const val"
 
     val willNotCompute = "Will not compute"
+}
+
+object ObjectWithConst {
+    const val a = 100
+    const val b = concat("Value in a: ", a)
+    val nonConst = concat("Non const, ", "still will be calculated")
+
+    @CompileTimeCalculation fun concat(first: String, second: Any) = "$first$second"
 }
 
 const val num = A().a
