b40ee028b021a9ab9c2afedca678264ded761961
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2020-06-10 23:04:36 +0300

    Add tests to check interpretation of String's plus extension function

--- compiler/testData/ir/interpreter/stringConcatenation.kt
+++ compiler/testData/ir/interpreter/stringConcatenation.kt
@@ -32,14 +32,34 @@ fun checkToStringCorrectness(value: Any, startSymbol: Char): Boolean {
 }
 
 @CompileTimeCalculation fun echo(value: String) = value
+@CompileTimeCalculation fun concat(first: String, second: Any) = "$first$second"
 
 const val constStr = echo("Success")
-const val concat1 = echo("String concatenation example: $constStr")
-const val concat2 = echo("String concatenation example with primitive: ${1}")
-const val concat3 = echo("String concatenation example with primitive and explicit toString call: ${1.toString()}")
+const val concat1 = concat("String concatenation example: ", constStr)
+const val concat2 = concat("String concatenation example with primitive: ", 1)
+const val concat3 = concat("String concatenation example with primitive and explicit toString call: ", 1.toString())
 const val concat4 = "String concatenation example with function that return primitive: ${getDoubledValue(10)}"
 const val concat5 = "String concatenation example with A class: ${A()}"
 const val concat6 = "String concatenation example with B class, where toString is FAKE_OVERRIDDEN: ${B()}"
 const val concat7 = "String concatenation example with B class and explicit toString call: ${B().toString()}"
 const val concat8 = "String concatenation example with C class, where toString isn't present; is it correct: ${checkToStringCorrectness(C(), 'C')}"
 const val concat9 = "String concatenation example with D class, where toString is taken from Any; is it correct: ${checkToStringCorrectness(D(), 'D')}"
+
+const val concat10 = "String plus example with A class: " + A()
+const val concat11 = "String plus example with B class, where toString is FAKE_OVERRIDDEN: " + B()
+
+const val concatLambda1 = "" + fun(): String = ""
+const val concatLambda2 = "" + (fun(): String = "").toString()
+const val concatLambda3 = "" + fun(): String = "Some string"
+const val concatLambda4 = "" + fun(i: Int): String = ""
+const val concatLambda5 = "" + fun(i: Int?): String? = ""
+const val concatLambda6 = "" + { i: Int -> "" }
+const val concatLambda7 = "" + {  }
+const val concatLambda8 = "" + { i: Int, b: Double, c: String ->  }
+const val concatLambda9 = "".let {
+    val lambdaWith: Double.(Int) -> String = { "A" }
+    lambdaWith.toString()
+}
+
+const val extensionPlus1 = "Null as string = " + null
+const val extensionPlus2 = "Null as string = ${null.toString()}"
