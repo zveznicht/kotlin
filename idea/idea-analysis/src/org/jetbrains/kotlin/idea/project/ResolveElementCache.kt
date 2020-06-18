1901f30903d5f6ed4368fd9532eaeef0443e9fc7
Vladimir Ilmov <vladimir.ilmov@jetbrains.com>
2020-06-18 18:48:00 +0200

    (Annotation) resolve test

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/project/ResolveElementCache.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/project/ResolveElementCache.kt
@@ -303,7 +303,7 @@ class ResolveElementCache(
     }
 
     private fun findElementOfAdditionalResolve(element: KtElement, bodyResolveMode: BodyResolveMode): KtElement? {
-        if (element is KtAnnotationEntry && bodyResolveMode == BodyResolveMode.PARTIAL_NO_ADDITIONAL)
+        if (element is KtAnnotationEntry)
             return element
 
         val elementOfAdditionalResolve = KtPsiUtil.getTopmostParentOfTypes(
@@ -530,6 +530,7 @@ class ResolveElementCache(
         val declaration = modifierList?.getParentOfType<KtDeclaration>(true)
         if (declaration != null) {
             doResolveAnnotations(getAnnotationsByDeclaration(resolveSession, modifierList, declaration))
+            forceResolveAnnotationsInside(modifierList)
         } else {
             val fileAnnotationList = ktAnnotationEntry.getParentOfType<KtFileAnnotationList>(true)
             if (fileAnnotationList != null) {
