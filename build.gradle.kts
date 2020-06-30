7872cd6a2a6950ee07deb1652c622893a13cb583
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2020-07-01 01:41:17 +0300

    Build: Add hacky workaround for retrying PublishToMavenRepository tasks
    It's supposed to help with `Caused by: org.apache.http.NoHttpResponseException: api.bintray.com:443 failed to respond`

--- build.gradle.kts
+++ build.gradle.kts
@@ -453,6 +453,8 @@ allprojects {
         }
     }
 
+    configurePublishingRetry()
+
     tasks {
         register("listArchives") { listConfigurationContents("archives") }
 
@@ -1072,6 +1074,37 @@ if (disableVerificationTasks) {
                 logger.info("DISABLED: '$it'")
                 it.enabled = false
             }
+        }
+    }
+}
+
+fun Project.configurePublishingRetry() {
+    val publishingAttempts = findProperty("kotlin.build.publishing.attempts")?.toString()?.toInt()
+
+    fun retry(attempts: Int, action: () -> Unit): Boolean {
+        repeat(attempts) {
+            try {
+                action()
+                return true
+            } catch (e: Throwable) {
+                e.printStackTrace()
+            }
+        }
+        return false
+    }
+
+    fun <T: Task> T.configureRetry(attempts: Int, taskAction: T.() -> Unit) {
+        doFirst {
+            if (retry(attempts) { taskAction() })
+                throw StopExecutionException()
+            else
+                error("Number of attempts ($attempts) exceeded for ${project.path}:$name")
+        }
+    }
+
+    if (publishingAttempts != null && publishingAttempts > 1) {
+        tasks.withType<PublishToMavenRepository> {
+            configureRetry(publishingAttempts, PublishToMavenRepository::publish)
         }
     }
 }
