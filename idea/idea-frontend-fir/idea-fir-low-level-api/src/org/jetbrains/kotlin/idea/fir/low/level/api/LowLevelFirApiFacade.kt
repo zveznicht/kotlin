68f7edeb86b5e04c07cde7309b1650f9723abe37
Roman Golyshev <roman.golyshev@jetbrains.com>
2020-07-01 14:23:00 +0300

    Completion with FIR (prototype, work-in-progress)

--- idea/idea-frontend-fir/idea-fir-low-level-api/src/org/jetbrains/kotlin/idea/fir/low/level/api/LowLevelFirApiFacade.kt
+++ idea/idea-frontend-fir/idea-fir-low-level-api/src/org/jetbrains/kotlin/idea/fir/low/level/api/LowLevelFirApiFacade.kt
@@ -7,12 +7,34 @@ package org.jetbrains.kotlin.idea.fir.low.level.api
 
 import org.jetbrains.kotlin.diagnostics.Diagnostic
 import org.jetbrains.kotlin.fir.FirElement
+import org.jetbrains.kotlin.fir.declarations.FirFile
+import org.jetbrains.kotlin.fir.declarations.FirFunction
 import org.jetbrains.kotlin.fir.declarations.FirResolvePhase
 import org.jetbrains.kotlin.psi.KtElement
+import org.jetbrains.kotlin.psi.KtNamedFunction
 
 object LowLevelFirApiFacade {
     fun getOrBuildFirFor(element: KtElement, phase: FirResolvePhase): FirElement =
         element.getOrBuildFir(element.firResolveState(), phase)
+
+    fun getFirOfClosestParent(element: KtElement): FirElement? = element.getFirOfClosestParent(element.firResolveState())?.second
+
+    fun buildFunctionWithResolvedBody(
+        firFile: FirFile,
+        element: KtNamedFunction,
+        phase: FirResolvePhase = FirResolvePhase.BODY_RESOLVE
+    ): FirFunction<*> {
+        val state = element.firResolveState()
+        val firIdeProvider = firFile.session.firIdeProvider
+        val builtFunction = firIdeProvider.buildFunctionWithBody(element)
+
+        return builtFunction.apply {
+            runResolve(firFile, firIdeProvider, phase, state)
+
+            // TODO this PSI caching should be somewhere else
+            state.recordElementsFrom(this)
+        }
+    }
 
     fun getDiagnosticsFor(element: KtElement): Collection<Diagnostic> {
         val file = element.containingKtFile
