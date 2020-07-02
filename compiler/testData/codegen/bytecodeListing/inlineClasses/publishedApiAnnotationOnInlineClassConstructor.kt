b0db7ff102d9e3df26031f464ce41c841ebd6d02
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2020-07-02 18:23:04 +0300

    --- testdata

--- compiler/testData/codegen/bytecodeListing/inlineClasses/publishedApiAnnotationOnInlineClassConstructor.kt
+++ compiler/testData/codegen/bytecodeListing/inlineClasses/publishedApiAnnotationOnInlineClassConstructor.kt
@@ -1,4 +1,5 @@
 // !LANGUAGE: +InlineClasses
+// IGNORE_BACKEND: JVM
 
 @Suppress("NON_PUBLIC_PRIMARY_CONSTRUCTOR_OF_INLINE_CLASS")
 inline class Z @PublishedApi internal constructor(val value: Int)
