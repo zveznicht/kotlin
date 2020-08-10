00a42d378f83a096751c2c00bf16bc2aac080668
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2020-08-10 12:24:54 +0200

    Don't generate implicit overrides delegating to Java defaults

---
+++ compiler/testData/codegen/box/jvm8/javaDefaults/kt40920_java.kt
@@ -0,0 +1,27 @@
+// !JVM_DEFAULT_MODE: disable
+// JVM_TARGET: 1.8
+// TARGET_BACKEND: JVM
+// IGNORE_BACKEND_FIR: JVM_IR
+// FILE: Base.java
+
+public interface Base {
+    default String test() {
+        return "Base";
+    }
+}
+
+
+// FILE: main.kt
+// WITH_RUNTIME
+
+interface LeftBase : Base
+interface Right : Base {
+    override fun test(): String = "OK"
+}
+
+interface Child : LeftBase, Right
+
+
+fun box(): String {
+    return object : Child {}.test()
+}
---
+++ compiler/testData/codegen/box/jvm8/javaDefaults/kt40920_java2.kt
@@ -0,0 +1,29 @@
+// !JVM_DEFAULT_MODE: disable
+// JVM_TARGET: 1.8
+// TARGET_BACKEND: JVM
+// IGNORE_BACKEND_FIR: JVM_IR
+// FILE: Base.java
+
+public interface Base {
+    default String test() {
+        return "Base";
+    }
+}
+
+// FILE: Left.java
+
+public interface Left extends Base {
+}
+
+// FILE: main.kt
+// WITH_RUNTIME
+
+interface Right : Base {
+    override fun test(): String = "OK"
+}
+
+interface Child : Left, Right
+
+fun box(): String {
+    return object : Child {}.test()
+}
