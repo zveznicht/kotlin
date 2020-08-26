a0ca7b28e669ef13981b0a5dc71b37ce3abb9ca6
Denis Zharkov <denis.zharkov@jetbrains.com>
2020-08-26 10:11:19 +0300

    FIR: Prepare refactoring of phasedFir
    - Rename PhasedFirFileResolver -> FirPhaseManager
    - Move semantically significant parts to a component

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/FirPhaseManager.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/FirPhaseManager.kt
@@ -5,20 +5,18 @@
 
 package org.jetbrains.kotlin.fir.resolve.transformers
 
+import org.jetbrains.kotlin.fir.FirSession
+import org.jetbrains.kotlin.fir.FirSessionComponent
 import org.jetbrains.kotlin.fir.FirSymbolOwner
 import org.jetbrains.kotlin.fir.declarations.FirDeclaration
 import org.jetbrains.kotlin.fir.declarations.FirResolvePhase
 import org.jetbrains.kotlin.fir.symbols.AbstractFirBasedSymbol
 
-fun <D> AbstractFirBasedSymbol<D>.phasedFir(
-    requiredPhase: FirResolvePhase = FirResolvePhase.DECLARATIONS
-): D where D : FirDeclaration, D : FirSymbolOwner<D> {
-    val result = this.fir
-    val resolver = fir.session.phaseManager
-        ?: error("phasedFirFileResolver should be defined when working with FIR in phased mode")
-
-    resolver.ensureResolved(this, requiredPhase)
-
-    return result
+abstract class FirPhaseManager : FirSessionComponent {
+    abstract fun <D> ensureResolved(
+        symbol: AbstractFirBasedSymbol<D>,
+        requiredPhase: FirResolvePhase = FirResolvePhase.DECLARATIONS
+    ) where D : FirDeclaration, D : FirSymbolOwner<D>
 }
 
+internal val FirSession.phaseManager: FirPhaseManager? by FirSession.nullableSessionComponentAccessor()
---
+++ idea/idea-frontend-fir/idea-fir-low-level-api/src/org/jetbrains/kotlin/idea/fir/low/level/api/IdeFirPhaseManager.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.idea.fir.low.level.api
+
+import org.jetbrains.kotlin.fir.FirSymbolOwner
+import org.jetbrains.kotlin.fir.declarations.FirDeclaration
+import org.jetbrains.kotlin.fir.declarations.FirResolvePhase
+import org.jetbrains.kotlin.fir.render
+import org.jetbrains.kotlin.fir.resolve.firProvider
+import org.jetbrains.kotlin.fir.resolve.transformers.FirPhaseManager
+import org.jetbrains.kotlin.fir.symbols.AbstractFirBasedSymbol
+import org.jetbrains.kotlin.fir.symbols.impl.FirCallableSymbol
+import org.jetbrains.kotlin.fir.symbols.impl.FirClassLikeSymbol
+import org.jetbrains.kotlin.idea.fir.low.level.api.file.builder.FirFileBuilder
+import org.jetbrains.kotlin.idea.fir.low.level.api.file.builder.ModuleFileCache
+
+internal class IdeFirPhaseManager(
+    private val firFileBuilder: FirFileBuilder,
+    private val cache: ModuleFileCache
+) : FirPhaseManager() {
+    override fun <D> ensureResolved(
+        symbol: AbstractFirBasedSymbol<D>,
+        requiredPhase: FirResolvePhase
+    ) where D : FirDeclaration, D : FirSymbolOwner<D> {
+        val result = symbol.fir
+        val availablePhase = result.resolvePhase
+        if (availablePhase >= requiredPhase) return
+        // NB: we should use session from symbol here, not transformer session (important for IDE)
+        val provider = symbol.fir.session.firProvider
+
+        require(provider.isPhasedFirAllowed) {
+            "Incorrect resolvePhase: actual: $availablePhase, expected: $requiredPhase\n For: ${symbol.fir.render()}"
+        }
+
+        val containingFile = when (symbol) {
+            is FirCallableSymbol<*> -> provider.getFirCallableContainerFile(symbol)
+            is FirClassLikeSymbol<*> -> provider.getFirClassifierContainerFile(symbol)
+            else -> null
+        } ?: throw AssertionError("Cannot get container file by symbol: $symbol (${result.render()})")
+
+        firFileBuilder.runResolve(containingFile, cache, availablePhase, requiredPhase)
+    }
+}
