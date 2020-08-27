398eb6113f01ac00ae881c9265e77f4f70ab8bc4
Roman Artemev <roman.artemev@jetbrains.com>
2020-08-27 10:58:56 +0300

    review fix. Delegation detector

--- compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/IrElementTransformerVoidWithContext.kt
+++ compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/IrElementTransformerVoidWithContext.kt
@@ -167,7 +167,7 @@ abstract class IrElementVisitorVoidWithContext : IrElementVisitorVoid {
     }
 
     final override fun visitField(declaration: IrField) {
-        val isDelegated = declaration.correspondingPropertySymbol == null
+        val isDelegated = declaration.correspondingPropertySymbol?.owner?.isDelegated == true
         if (isDelegated) scopeStack.push(createScope(declaration))
         visitFieldNew(declaration)
         if (isDelegated) scopeStack.pop()
