3d9eef21593f1986661ae1db9bca279439f08ae6
Roman Artemev <roman.artemev@jetbrains.com>
2020-08-31 12:44:36 +0300

    fix review test decl names

--- compiler/testData/codegen/boxError/syntax/incorectLexicalName.kt
+++ compiler/testData/codegen/boxError/syntax/incorectLexicalName.kt
@@ -7,7 +7,16 @@
 
 fun 124gga() {}
 
+val 481gu: Boolean = true
+
+var 981llj): Int = 42
+
+// just check if it is compiled
+class **jhghssk
+
 fun foo() { 124gga() }
+fun bar() { return 481gu }
+fun qux() { 981llj) = 481 }
 
 // FILE: b.kt
 
@@ -15,7 +24,15 @@ fun box(): String {
     try {
         foo()
     } catch (e: IllegalStateException) {
-        return "OK"
+        try {
+            bar()
+        } catch (e: IllegalStateException) {
+            try {
+                qux()
+            } catch (e: IllegalStateException) {
+                return "OK"
+            }
+        }
     }
     return "FAIL"
 }
