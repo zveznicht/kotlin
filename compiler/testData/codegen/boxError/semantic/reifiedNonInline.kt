4b4661661d4ecc6a7ab543ce5ac620d438f171ec
Roman Artemev <roman.artemev@jetbrains.com>
2020-08-31 13:01:17 +0300

    fix review test reified test

--- compiler/testData/codegen/boxError/semantic/reifiedNonInline.kt
+++ compiler/testData/codegen/boxError/semantic/reifiedNonInline.kt
@@ -4,12 +4,20 @@
 
 fun <reified T> bar(t: T) = t
 
+fun <reified T> qux() = T::class
+
 fun foo(): String {
     return bar<String>("OK")
 }
 
+fun dec() { qux() }
+
 // FILE: b.kt
 
 fun box(): String {
-    return foo()
+    try {
+        dec()
+    } catch (e: Throwable /*js ReferenceError*/) {
+        return foo()
+    }
 }
