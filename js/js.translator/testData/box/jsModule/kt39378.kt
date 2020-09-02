24287476a8529413db273bfb911bc219ae528738
Roman Artemev <roman.artemev@jetbrains.com>
2020-09-02 13:46:15 +0300

    [JS] Add test for KT-39378

---
+++ js/js.translator/testData/box/jsModule/kt39378.kt
@@ -0,0 +1,36 @@
+// EXPECTED_REACHABLE_NODES: 1238
+
+// MODULE: lib
+// FILE: l.kt
+
+package l
+
+external interface E {
+    var ss: Array<String> // Effectively external property.
+}
+
+@JsModule("lib")
+@JsNonModule
+external fun ee(): E
+
+fun foo(): String {
+
+    val e = ee()
+
+    var result =  ""
+
+    for (s in e.ss) {
+        result += s
+    }
+
+    return result
+}
+
+// MODULE: main(lib)
+// FILE: m.kt
+
+import l.*
+
+fun box(): String {
+    return foo()
+}
