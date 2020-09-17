3691d0e0a149f0585a8eaed0c8a045525bdf0140
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2020-09-17 18:32:07 +0300

    JVM_IR KT-40304 KT-41998 special handling for 'removeAt'

--- compiler/tests/org/jetbrains/kotlin/codegen/BlackBoxCodegenTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/codegen/BlackBoxCodegenTestGenerated.java
@@ -5028,6 +5028,11 @@ public class BlackBoxCodegenTestGenerated extends AbstractBlackBoxCodegenTest {
             runTest("compiler/testData/codegen/box/collections/readOnlyMap.kt");
         }
 
+        @TestMetadata("removeAtBridgeClashWithJava.kt")
+        public void testRemoveAtBridgeClashWithJava() throws Exception {
+            runTest("compiler/testData/codegen/box/collections/removeAtBridgeClashWithJava.kt");
+        }
+
         @TestMetadata("removeAtInt.kt")
         public void testRemoveAtInt() throws Exception {
             runTest("compiler/testData/codegen/box/collections/removeAtInt.kt");
