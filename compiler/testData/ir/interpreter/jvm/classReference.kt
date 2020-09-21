1cd12d350de78a05525e69cae3819126dc6277c2
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2020-09-21 13:11:55 +0300

    Add new test cases in classReference.kt file

--- compiler/testData/ir/interpreter/jvm/classReference.kt
+++ compiler/testData/ir/interpreter/jvm/classReference.kt
@@ -2,6 +2,8 @@
 
 package test
 
+import kotlin.reflect.KFunction
+
 @CompileTimeCalculation
 class A(val a: Int, val b: String) {
     val String.propertyWithExtension: Int
@@ -10,7 +12,30 @@ class A(val a: Int, val b: String) {
     fun Int.funWithExtension(other: Int) = this + other
 }
 
-const val simpleName = A::class.simpleName!!
-const val qualifiedName = A::class.qualifiedName!!
-const val members = A::class.members.joinToString()
-const val constructors = A::class.constructors.joinToString()
+const val aSimpleName = A::class.simpleName!!
+const val aQualifiedName = A::class.qualifiedName!!
+const val aMembers = A::class.members.joinToString()
+const val aConstructors = A::class.constructors.joinToString()
+const val aVisibility = A::class.visibility.toString()
+const val aSupertypes = A::class.supertypes.joinToString()
+
+@CompileTimeCalculation
+interface Base<T>
+
+@CompileTimeCalculation
+class B<T, E : T, D : Any>(val prop: T) : Base<T> {
+    fun get(): T = prop
+
+    fun getThis(): B<T, out E, in D> = this
+
+    fun <E : Number> withTypeParameter(num: E) = num.toString()
+}
+
+const val bMembers = B::class.members.joinToString()
+const val bTypeParameters = B::class.typeParameters.joinToString()
+const val bSupertypes = B::class.supertypes.joinToString()
+const val bReturnType1 = B::class.members.toList()[1].returnType.toString()
+const val bReturnType2 = B::class.members.toList()[3].returnType.toString()
+
+const val arguments1 = (B<Number, Double, Int>(1)::getThis as KFunction<*>).returnType.arguments.joinToString()
+const val arguments2 = (arrayOf(1)::iterator as KFunction<*>).returnType.arguments.joinToString()
