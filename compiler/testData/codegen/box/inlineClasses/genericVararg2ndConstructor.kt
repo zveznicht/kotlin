94e099a4cedd5efebadfc386b308ba11c8be8e3f
Roman Artemev <roman.artemev@jetbrains.com>
2020-09-22 17:57:55 +0300

    [IR BE] Fix inline class lowering
     - provide additional type parameters from context when transforming
     constructor into static function.
     - fix KT-41771
     - add test

---
+++ compiler/testData/codegen/box/inlineClasses/genericVararg2ndConstructor.kt
@@ -0,0 +1,12 @@
+// KJS_WITH_FULL_RUNTIME
+
+
+// KT-41771
+
+inline class Polynomial<T : Any>(val coefficients: List<T>) {
+    constructor(vararg coefficients: T) : this(coefficients.toList())
+}
+fun box(): String {
+    val p = Polynomial("FAIL1", "OK", "FAIL2")
+    return p.coefficients[1]
+}
