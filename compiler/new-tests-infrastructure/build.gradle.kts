0efbf93122d544a8aada30d80e404b0ca6d46800
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-10-02 13:01:27 +0300

    [TEST] Add module for new test infrastructure

---
+++ compiler/new-tests-infrastructure/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    kotlin("jvm")
+    id("jps-compatible")
+}
+
+dependencies {
+    testImplementation(project(":compiler:fir:entrypoint"))
+
+    testImplementation(platform("org.junit:junit-bom:5.7.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    testImplementation(intellijDep()) {
+        // This dependency is needed only for FileComparisonFailure
+        includeJars("idea_rt", rootProject = rootProject)
+        isTransitive = false
+    }
+
+    testImplementation(intellijDep()) {
+        includeJars(
+            "commons-lang3",
+            "commons-io",
+            rootProject = rootProject
+        )
+    }
+
+    // This is needed only for using FileComparisonFailure, which relies on JUnit 3 classes
+    testRuntimeOnly(commonDep("junit:junit"))
+}
+
+sourceSets {
+    "main" { none() }
+    "test" { projectDefault() }
+}
+
+projectTest(parallel = true) {
+    dependsOn(":dist")
+    workingDir = rootDir
+    jvmArgs!!.removeIf { it.contains("-Xmx") }
+    maxHeapSize = "3g"
+
+    useJUnitPlatform()
+}
+
+testsJar()
