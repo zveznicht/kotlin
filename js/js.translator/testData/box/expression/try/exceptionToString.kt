4971d29e0c9ef74f54f80ead099e11cc3fcd1e7a
Roman Artemev <roman.artemev@jetbrains.com>
2020-10-06 16:24:13 +0300

    [JS BE] Merge Legacy and IR BE exceptions-related test data
     - regenerate tests

--- js/js.translator/testData/box/expression/try/exceptionToString.kt
+++ js/js.translator/testData/box/expression/try/exceptionToString.kt
@@ -1,4 +1,3 @@
-// IGNORE_BACKEND: JS
 // EXPECTED_REACHABLE_NODES: 1298
 package foo
 
@@ -24,25 +23,25 @@ fun check(e: Throwable, expectedString: String) {
 }
 
 fun box(): String {
-    check(Throwable(), "Throwable")
+    check(Throwable(), "Throwable: null")
     check(Throwable("ccc"), "Throwable: ccc")
     check(Throwable(Throwable("ddd")), "Throwable: Throwable: ddd")
-    check(Exception(), "Exception")
+    check(Exception(), "Exception: null")
     check(Exception("bbb"), "Exception: bbb")
     check(Exception(Exception("ccc")), "Exception: Exception: ccc")
-    check(AssertionError(), "AssertionError")
-    check(AssertionError(null), "AssertionError")
+    check(AssertionError(), "AssertionError: null")
+    check(AssertionError(null), "AssertionError: null")
     check(AssertionError("bbb"), "AssertionError: bbb")
     check(AssertionError(Exception("ccc")), "AssertionError: Exception: ccc")
-    check(MyException(), "MyException")
+    check(MyException(), "MyException: null")
     check(MyException("aaa"), "MyException: aaa")
-    check(MyException2(), "MyException2")
+    check(MyException2(), "MyException2: null")
     check(MyException2("aaa"), "MyException2: aaa")
-    check(MyException3(), "MyException3")
+    check(MyException3(), "MyException3: null")
     check(MyException3("aaa"), "MyException3: aaa")
 
     check(MyException4(), "MyException4")
-    check(MyException4(AssertionError()), "MyException4: AssertionError")
+    check(MyException4(AssertionError()), "MyException4: AssertionError: null")
 
     check(MyException5(), "MyException5")
     check(MyException5(MyException5()), "MyException5: MyException5")
