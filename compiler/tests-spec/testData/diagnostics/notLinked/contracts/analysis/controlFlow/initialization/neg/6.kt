bcf865be51141a525bec233b8d642de9dc3df818
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2020-10-12 01:21:28 +0200

    fixup! Extract effect from lambda argument if it is in parentheses

--- compiler/tests-spec/testData/diagnostics/notLinked/contracts/analysis/controlFlow/initialization/neg/6.kt
+++ compiler/tests-spec/testData/diagnostics/notLinked/contracts/analysis/controlFlow/initialization/neg/6.kt
@@ -10,16 +10,4 @@
  * HELPERS: contractFunctions
  */
 
-// TESTCASE NUMBER: 1
-fun case_1() {
-    val value_1: Int
-    funWithExactlyOnceCallsInPlace({ value_1 = 11 })
-    value_1.inc()
-}
-
-// TESTCASE NUMBER: 2
-fun case_2() {
-    var value_1: Int
-    funWithAtLeastOnceCallsInPlace({ value_1 = 11 })
-    value_1.inc()
-}
+// The file is here so FIR tests will run
