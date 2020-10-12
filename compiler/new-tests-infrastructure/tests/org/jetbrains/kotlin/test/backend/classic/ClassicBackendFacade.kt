09a4e9977b1fb6f0b6186c1a86e00b8847a00ca5
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-10-12 14:28:05 +0300

    [TEST] Implement ClassicJvmBackendFacade

---
+++ compiler/new-tests-infrastructure/tests/org/jetbrains/kotlin/test/backend/classic/ClassicBackendFacade.kt
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.test.backend.classic
+
+import org.jetbrains.kotlin.test.model.BackendFacade
+import org.jetbrains.kotlin.test.model.ResultingArtifact
+
+abstract class ClassicBackendFacade<R : ResultingArtifact.Binary> : BackendFacade<ClassicBackendInputInfo, R>()
---
+++ compiler/new-tests-infrastructure/tests/org/jetbrains/kotlin/test/backend/classic/ClassicJvmBackendFacade.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.test.backend.classic
+
+import org.jetbrains.kotlin.codegen.ClassBuilderFactories
+import org.jetbrains.kotlin.codegen.DefaultCodegenFactory
+import org.jetbrains.kotlin.codegen.KotlinCodegenFacade
+import org.jetbrains.kotlin.codegen.state.GenerationState
+import org.jetbrains.kotlin.test.components.ConfigurationComponents
+import org.jetbrains.kotlin.test.model.ResultingArtifact
+import org.jetbrains.kotlin.test.model.TestModule
+
+class ClassicJvmBackendFacade(
+    val configurationComponents: ConfigurationComponents
+) : ClassicBackendFacade<ResultingArtifact.Binary.Jvm>() {
+    override fun produce(module: TestModule, initialInfo: ClassicBackendInputInfo): ResultingArtifact.Binary.Jvm {
+        val environment = configurationComponents.kotlinCoreEnvironmentProvider.getKotlinCoreEnvironment(module)
+        val compilerConfiguration = environment.configuration
+        val (psiFiles, bindingContext, moduleDescriptor, project, languageVersionSettings) = initialInfo
+        // TODO: add configuring classBuilderFactory
+        val generationState = GenerationState.Builder(
+            project,
+            ClassBuilderFactories.TEST,
+            moduleDescriptor,
+            bindingContext,
+            psiFiles.toList(),
+            compilerConfiguration
+        ).codegenFactory(DefaultCodegenFactory).build()
+
+        KotlinCodegenFacade.compileCorrectFiles(generationState)
+
+        return ResultingArtifact.Binary.Jvm(generationState.factory)
+    }
+}
