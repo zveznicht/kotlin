8a3f49106c310e5724d238c09358d1efe3246003
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2020-10-14 23:05:50 +0300

    Do not hide synthetic properties except `isEmpty` from Java
     In order to hide synthetic property isEmpty from JDK 15, there was
     added additional logic in 5cc12b49fcaf828c4bf4279293b1c8a1f61a114f but
     it also lead to the fact that now it's impossible to call synthetic
     property `declaringClass` on `Enum` as it's in the same list as method
     isEmpty(). Note that it's questionable behavior, probably we should
     also hide `declaringClass` as well but for now we turn it back to
     preserve compatibility
    
     #KT-42467 Fixed

---
+++ compiler/testData/codegen/box/enum/declaringClassOnEnumObject.kt
@@ -0,0 +1,12 @@
+// TARGET_BACKEND: JVM
+
+enum class KEnum { A }
+
+fun test(e: KEnum): String {
+    return e.declaringClass.toString()
+}
+
+fun box(): String {
+    val result = test(KEnum.A)
+    return if (result == "class KEnum") "OK" else "fail: $result"
+}
