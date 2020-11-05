7af33e6e1478e3a8f16ebcf760f08ad882c396cd
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-11-05 14:25:55 +0300

    [TEST] Render regular debug infos in classic diagnostics handler

--- compiler/new-tests-infrastructure/tests/org/jetbrains/kotlin/test/frontend/classic/handlers/ClassicDiagnosticsHandler.kt
+++ compiler/new-tests-infrastructure/tests/org/jetbrains/kotlin/test/frontend/classic/handlers/ClassicDiagnosticsHandler.kt
@@ -5,17 +5,23 @@
 
 package org.jetbrains.kotlin.test.frontend.classic.handlers
 
+import org.jetbrains.kotlin.checkers.utils.CheckerTestUtil
+import org.jetbrains.kotlin.checkers.utils.DiagnosticsRenderingConfiguration
 import org.jetbrains.kotlin.codeMetaInfo.model.DiagnosticCodeMetaInfo
-import org.jetbrains.kotlin.codeMetaInfo.renderConfigurations.DiagnosticCodeMetaInfoRenderConfiguration
+import org.jetbrains.kotlin.config.JVMConfigurationKeys
+import org.jetbrains.kotlin.descriptors.impl.ModuleDescriptorImpl
+import org.jetbrains.kotlin.diagnostics.Diagnostic
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.resolve.calls.smartcasts.DataFlowValueFactoryImpl
 import org.jetbrains.kotlin.test.frontend.classic.ClassicFrontendSourceArtifacts
+import org.jetbrains.kotlin.test.model.TestFile
 import org.jetbrains.kotlin.test.model.TestModule
 import org.jetbrains.kotlin.test.services.GlobalMetadataInfoHandler
 import org.jetbrains.kotlin.test.services.TestServices
 import org.jetbrains.kotlin.test.services.globalMetadataInfoHandler
+import org.jetbrains.kotlin.test.services.kotlinCoreEnvironmentProvider
 
 class ClassicDiagnosticsHandler(testServices: TestServices) : ClassicFrontendAnalysisHandler(testServices) {
-    private val renderConfigurationNoArgs = DiagnosticCodeMetaInfoRenderConfiguration().apply { renderParams = false }
-    private val renderConfigurationWithArgs = DiagnosticCodeMetaInfoRenderConfiguration().apply { renderParams = true }
     private val globalMetadataInfoHandler: GlobalMetadataInfoHandler
         get() = testServices.globalMetadataInfoHandler
 
@@ -25,20 +31,52 @@ class ClassicDiagnosticsHandler(testServices: TestServices) : ClassicFrontendAna
             it.psiFile
         }
 
+        val configuration = DiagnosticsRenderingConfiguration(
+            platform = null,
+            withNewInference = true,
+            languageVersionSettings = info.languageVersionSettings,
+            skipDebugInfoDiagnostics = testServices.kotlinCoreEnvironmentProvider.getCompilerConfiguration(module).getBoolean(JVMConfigurationKeys.IR)
+        )
+
         for (file in module.files) {
             val ktFile = info.psiFiles[file] ?: continue
             val diagnostics = diagnosticsPerFile[ktFile] ?: continue
-            val diagnosticsMetadataInfos = diagnostics.flatMap { diagnostic ->
-                diagnostic.textRanges.map { range ->
-                    val metaInfo = DiagnosticCodeMetaInfo(range, renderConfigurationNoArgs, diagnostic)
-                    val existing = globalMetadataInfoHandler.getExistingMetaInfosForActualMetadata(file, metaInfo)
-                    if (existing.any { it.description != null }) {
-                        metaInfo.replaceRenderConfiguration(renderConfigurationWithArgs)
-                    }
-                    metaInfo
-                }
+            for (diagnostic in diagnostics) {
+                globalMetadataInfoHandler.addMetadataInfosForFile(file, diagnostic.toMetaInfo(file))
             }
-            globalMetadataInfoHandler.addMetadataInfosForFile(file, diagnosticsMetadataInfos)
+            processDebugInfoDiagnostics(configuration, file, ktFile, info)
+        }
+    }
+
+    private fun Diagnostic.toMetaInfo(
+        file: TestFile
+    ): List<DiagnosticCodeMetaInfo> = textRanges.map { range ->
+        val metaInfo = DiagnosticCodeMetaInfo(range, ClassicMetaInfoUtils.renderDiagnosticNoArgs, this)
+        val existing = globalMetadataInfoHandler.getExistingMetaInfosForActualMetadata(file, metaInfo)
+        if (existing.any { it.description != null }) {
+            metaInfo.replaceRenderConfiguration(ClassicMetaInfoUtils.renderDiagnosticWithArgs)
+        }
+        metaInfo
+    }
+
+    private fun processDebugInfoDiagnostics(
+        configuration: DiagnosticsRenderingConfiguration,
+        file: TestFile,
+        ktFile: KtFile,
+        info: ClassicFrontendSourceArtifacts
+    ) {
+        val debugAnnotations = CheckerTestUtil.getDebugInfoDiagnostics(
+            ktFile,
+            info.analysisResult.bindingContext,
+            markDynamicCalls = true,
+            dynamicCallDescriptors = mutableListOf(),
+            configuration,
+            dataFlowValueFactory = DataFlowValueFactoryImpl(info.languageVersionSettings),
+            info.analysisResult.moduleDescriptor as ModuleDescriptorImpl,
+            diagnosedRanges = null
+        )
+        debugAnnotations.map { debugAnnotation ->
+            globalMetadataInfoHandler.addMetadataInfosForFile(file, debugAnnotation.diagnostic.toMetaInfo(file))
         }
     }
 
