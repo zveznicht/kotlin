98c4fecdec3f285e5635c3858e9c47c796f4e794
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-11-12 14:03:16 +0300

    [TEST] Use positioning strategy from FE 1.0 for reporting FIR diagnostics

--- compiler/new-tests-infrastructure/tests/org/jetbrains/kotlin/test/frontend/fir/handlers/FirDiagnosticCodeMetaInfo.kt
+++ compiler/new-tests-infrastructure/tests/org/jetbrains/kotlin/test/frontend/fir/handlers/FirDiagnosticCodeMetaInfo.kt
@@ -5,10 +5,15 @@
 
 package org.jetbrains.kotlin.test.frontend.fir.handlers
 
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.codeMetaInfo.model.CodeMetaInfo
 import org.jetbrains.kotlin.codeMetaInfo.renderConfigurations.AbstractCodeMetaInfoRenderConfiguration
+import org.jetbrains.kotlin.diagnostics.Diagnostic
+import org.jetbrains.kotlin.diagnostics.PositioningStrategy
 import org.jetbrains.kotlin.fir.analysis.diagnostics.FirDefaultErrorMessages
 import org.jetbrains.kotlin.fir.analysis.diagnostics.FirDiagnostic
+import org.jetbrains.kotlin.fir.psi
 
 object FirMetaInfoUtils {
     val renderDiagnosticNoArgs = FirDiagnosticCodeMetaRenderConfiguration().apply { renderParams = false }
@@ -19,13 +24,23 @@ class FirDiagnosticCodeMetaInfo(
     val diagnostic: FirDiagnostic<*>,
     renderConfiguration: FirDiagnosticCodeMetaRenderConfiguration
 ) : CodeMetaInfo {
+    // TODO: this implementation is hacky and doesn't support proper ranges for light tree diagnostics
+    private val textRangeFromClassicDiagnostic: TextRange? = run {
+        val psi = diagnostic.element.psi ?: return@run null
+
+        @Suppress("UNCHECKED_CAST")
+        val positioningStrategy = diagnostic.factory.psiDiagnosticFactory.positioningStrategy as PositioningStrategy<PsiElement>
+        positioningStrategy.mark(psi).first()
+    }
+
     override var renderConfiguration: FirDiagnosticCodeMetaRenderConfiguration = renderConfiguration
         private set
 
     override val start: Int
-        get() = diagnostic.element.startOffset
+        get() = textRangeFromClassicDiagnostic?.startOffset ?: diagnostic.element.startOffset
+
     override val end: Int
-        get() = diagnostic.element.endOffset
+        get() = textRangeFromClassicDiagnostic?.endOffset ?: diagnostic.element.endOffset
 
     override val tag: String
         get() = renderConfiguration.getTag(this)
