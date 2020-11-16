67fed1136fe3652e4cbd0d7ce89b2ec889c0d8a6
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-11-16 12:20:15 +0300

    [FE] Collect inheritors of sealed classes from new places in `computeSealedSubclasses`
    #KT-13495

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/fir/FirOldFrontendDiagnosticsTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/fir/FirOldFrontendDiagnosticsTestGenerated.java
@@ -20763,6 +20763,11 @@ public class FirOldFrontendDiagnosticsTestGenerated extends AbstractFirOldFronte
             runTest("compiler/testData/diagnostics/tests/sealed/ExhaustiveWhenWithElse.kt");
         }
 
+        @TestMetadata("ExhaustiveWithFreedom.kt")
+        public void testExhaustiveWithFreedom() throws Exception {
+            runTest("compiler/testData/diagnostics/tests/sealed/ExhaustiveWithFreedom.kt");
+        }
+
         @TestMetadata("Local.kt")
         public void testLocal() throws Exception {
             runTest("compiler/testData/diagnostics/tests/sealed/Local.kt");
@@ -20781,6 +20786,11 @@ public class FirOldFrontendDiagnosticsTestGenerated extends AbstractFirOldFronte
         @TestMetadata("NestedSealed.kt")
         public void testNestedSealed() throws Exception {
             runTest("compiler/testData/diagnostics/tests/sealed/NestedSealed.kt");
+        }
+
+        @TestMetadata("NestedSealedWithoutRestrictions.kt")
+        public void testNestedSealedWithoutRestrictions() throws Exception {
+            runTest("compiler/testData/diagnostics/tests/sealed/NestedSealedWithoutRestrictions.kt");
         }
 
         @TestMetadata("NeverConstructed.kt")
