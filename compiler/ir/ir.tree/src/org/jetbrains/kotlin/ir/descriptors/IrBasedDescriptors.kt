fbbec40df3c5f657fbc85b706e6051ca258a89ee
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2020-11-17 03:25:04 +0100

    Value classes: Add isValue property to class descriptors
    Reuse isInline flag in proto and IR.
    Check metadata version on deserialization.

--- compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/descriptors/IrBasedDescriptors.kt
+++ compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/descriptors/IrBasedDescriptors.kt
@@ -605,6 +605,9 @@ open class IrBasedClassDescriptor(owner: IrClass) : ClassDescriptor, IrBasedDecl
 
     override fun isFun() = owner.isFun
 
+    // In IR, inline and value are synonyms
+    override fun isValue() = owner.isInline
+
     override fun getThisAsReceiverParameter() = owner.thisReceiver?.toIrBasedDescriptor() as ReceiverParameterDescriptor
 
     override fun getUnsubstitutedPrimaryConstructor() =
@@ -704,6 +707,8 @@ open class IrBasedEnumEntryDescriptor(owner: IrEnumEntry) : ClassDescriptor, IrB
     override fun isInline() = false
 
     override fun isFun() = false
+
+    override fun isValue() = false
 
     override fun getThisAsReceiverParameter() = (owner.parent as IrClass).toIrBasedDescriptor().thisAsReceiverParameter
 
