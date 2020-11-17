b63655e8b7a60743cd3acb9395ecee334f487121
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2020-11-17 11:25:51 +0100

    ~ prelim module

---
+++ compiler/cli/cli-fir/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm")
+    id("jps-compatible")
+}
+
+dependencies {
+    compile(project(":compiler:util"))
+    compile(project(":compiler:cli-common"))
+    compile(project(":compiler:cli"))
+    compile(project(":compiler:cli-js"))
+    compile(project(":compiler:frontend"))
+    compile(project(":compiler:backend-common"))
+    compile(project(":compiler:ir.backend.common"))
+    compile(project(":compiler:frontend"))
+    compile(project(":compiler:backend.js"))
+    compile(project(":js:js.translator"))
+    compile(project(":js:js.serializer"))
+    compile(project(":js:js.dce"))
+    implementation(project(":compiler:fir:entrypoint"))
+
+    compileOnly(intellijCoreDep()) { includeJars("intellij-core") }
+}
+
+sourceSets {
+    "main" { projectDefault() }
+    "test" { projectDefault() }
+}
