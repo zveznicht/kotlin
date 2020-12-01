88adbac6deef6f47e43e250afc7a0a6a862e4d4a
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-12-01 10:37:50 +0300

    [FE] Rename FreedomForSealedClasses feature with more meaningful name

--- compiler/util/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
+++ compiler/util/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
@@ -142,7 +142,7 @@ enum class LanguageFeature(
     ForbidReferencingToUnderscoreNamedParameterOfCatchBlock(KOTLIN_1_5, kind = BUG_FIX),
     UseCorrectExecutionOrderForVarargArguments(KOTLIN_1_5, kind = BUG_FIX),
 
-    FreedomForSealedClasses(KOTLIN_1_5),
+    AllowSealedInheritorsInDifferentFilesOfSamePackage(KOTLIN_1_5),
     SealedInterfaces(KOTLIN_1_5),
 
     // Temporarily disabled, see KT-27084/KT-22379
