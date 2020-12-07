66c5c6c7054f6271e46919549ea7f7c50c50791e
Pavel Kirpichenkov <pavel.kirpichenkov@jetbrains.com>
2020-12-07 16:09:24 +0300

    Drop debug stack traces from module and built-ins
    They are not that useful in production and can be added locally when needed

--- core/descriptors/src/org/jetbrains/kotlin/descriptors/impl/ModuleDescriptorImpl.kt
+++ core/descriptors/src/org/jetbrains/kotlin/descriptors/impl/ModuleDescriptorImpl.kt
@@ -48,8 +48,6 @@ class ModuleDescriptorImpl @JvmOverloads constructor(
         this.capabilities[REFINER_CAPABILITY] = Ref(null)
     }
 
-    private val createdAt: Throwable = Throwable()
-
     private var dependencies: ModuleDependencies? = null
     private var packageFragmentProviderForModuleContent: PackageFragmentProvider? = null
 
