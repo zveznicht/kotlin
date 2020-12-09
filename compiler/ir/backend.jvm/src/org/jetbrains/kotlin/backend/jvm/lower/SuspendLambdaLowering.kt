7bb08a3ebd298d843ff046f3e5ca52d563ae6183
Iaroslav Postovalov <postovalovya@gmail.com>
2020-12-09 22:58:03 +0700

    Remove useless OptIn annotation

--- compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/lower/SuspendLambdaLowering.kt
+++ compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/lower/SuspendLambdaLowering.kt
@@ -143,7 +143,6 @@ private class SuspendLambdaLowering(context: JvmBackendContext) : SuspendLowerin
             }
         }
 
-    @OptIn(ObsoleteDescriptorBasedAPI::class)
     private fun generateContinuationClassForLambda(reference: IrFunctionReference, parent: IrDeclarationParent): IrClass =
         context.irFactory.buildClass {
             name = SpecialNames.NO_NAME_PROVIDED
