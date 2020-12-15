9299944549c80a4881c36d3701c4a5ea1d2a7525
Svyatoslav Kuzmich <svyatoslav.kuzmich@jetbrains.com>
2020-12-15 16:54:47 +0300

    [Wasm] Add inline property accessors optimization

--- compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/lower/optimizations/PropertyAccessorInlineLowering.kt
+++ compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/lower/optimizations/PropertyAccessorInlineLowering.kt
@@ -10,8 +10,9 @@ import org.jetbrains.kotlin.backend.common.CommonBackendContext
 import org.jetbrains.kotlin.backend.common.ir.isTopLevel
 import org.jetbrains.kotlin.backend.common.lower.createIrBuilder
 import org.jetbrains.kotlin.backend.common.lower.irBlock
-import org.jetbrains.kotlin.descriptors.Modality
 import org.jetbrains.kotlin.descriptors.DescriptorVisibilities
+import org.jetbrains.kotlin.descriptors.Modality
+import org.jetbrains.kotlin.ir.builders.irImplicitCast
 import org.jetbrains.kotlin.ir.declarations.*
 import org.jetbrains.kotlin.ir.expressions.*
 import org.jetbrains.kotlin.ir.expressions.impl.IrGetFieldImpl
@@ -83,8 +84,16 @@ class PropertyAccessorInlineLowering(private val context: CommonBackendContext) 
         private fun tryInlineSimpleGetter(call: IrCall, callee: IrSimpleFunction, backingField: IrField): IrExpression? {
             if (!isSimpleGetter(callee, backingField)) return null
 
-            return call.run {
+            val builder = context.createIrBuilder(call.symbol, call.startOffset, call.endOffset)
+
+            val getField = call.run {
                 IrGetFieldImpl(startOffset, endOffset, backingField.symbol, backingField.type, call.dispatchReceiver, origin)
+            }
+
+            return if (backingField.type != call.type) {
+                return builder.irImplicitCast(getField, call.type)
+            } else {
+                getField
             }
         }
 
