d0735e79693e92a9c2df580ae6893bb77c2cde56
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2020-12-17 21:46:01 +0100

    Update companionAccessingEnumValue with init blocks

--- compiler/testData/codegen/box/enum/companionAccessingEnumValue.kt
+++ compiler/testData/codegen/box/enum/companionAccessingEnumValue.kt
@@ -1,11 +1,32 @@
+private var logs = ""
+
 enum class Foo(val text: String) {
-    FOO("foo");
+    FOO("foo"),
+    BAR("bar"),
+    PING("foo");
+
+    init {
+        logs += "B"
+    }
+
     companion object {
         val first = values()[0]
+        init {
+            logs += first.text
+        }
+    }
+
+    init {
+        logs += "C"
     }
 }
 
 fun box(): String {
     Foo.FOO
-    return if (Foo.first === Foo.FOO) "OK" else "FAIL: ${Foo.first}"
+
+    if (Foo.first !== Foo.FOO) return "FAIL 0: ${Foo.first}"
+
+    if (logs != "BCBCBCfoo") return "FAIL 1: ${logs}"
+
+    return "OK"
 }
