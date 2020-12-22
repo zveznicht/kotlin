0227e0c171746088261d7ef85fb21edc856f86f5
Yunir Salimzyanov <yunir.salimzyanov@jetbrains.com>
2020-12-22 15:28:19 +0300

    --

--- compiler/tests-mutes/tc-integration/src/org/jetbrains/kotlin/test/mutes/MutedTestsSync.kt
+++ compiler/tests-mutes/tc-integration/src/org/jetbrains/kotlin/test/mutes/MutedTestsSync.kt
@@ -93,7 +93,7 @@ class LocallyMutedTests {
 
     private fun getMutedTestsFromDatabase(): Map<String, List<MutedTest>> {
         val mutedTestsMap = mutableMapOf<String, List<MutedTest>>()
-        val databaseDir = "../../tests"
+        val databaseDir = "../../../tests"
 
         val commonDatabaseFile = File(databaseDir, "mute-common.csv")
         mutedTestsMap[muteCommonTestKey] = flakyTests(commonDatabaseFile)
