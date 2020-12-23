223679cf0a122845c18fa43c4c95b8c3379936b6
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2020-12-23 15:25:45 +0300

    Extract long project wizard tests to separate TC configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -776,7 +776,6 @@ tasks {
 
     if (Ide.IJ()) {
         register("idea-new-project-wizard-tests") {
-            dependsOn("dist")
             dependsOn(
                 ":libraries:tools:new-project-wizard:test",
                 ":libraries:tools:new-project-wizard:new-project-wizard-cli:test",
@@ -849,7 +848,17 @@ tasks {
             ":generators:test"
         )
         if (Ide.IJ()) {
-            dependsOn("idea-new-project-wizard-tests")
+            dependsOn(
+                ":libraries:tools:new-project-wizard:test",
+                ":libraries:tools:new-project-wizard:new-project-wizard-cli:test",
+                ":idea:idea-new-project-wizard:test" // Temporary here. Remove after enabling builds for ideaNewProjectWizardTests
+            )
+        }
+    }
+
+    register("ideaNewProjectWizardTests") {
+        if (Ide.IJ()) {
+            dependsOn(":idea:idea-new-project-wizard:test")
         }
     }
 
