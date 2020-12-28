d49092f0f1259b8144aacd4acc82a275e1657851
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2020-12-28 23:28:19 +0100

    Forbid export of interfaces - add corresponding test

--- compiler/testData/diagnostics/testsWithJsStdLib/export/wrongExportedDeclaration.kt
+++ compiler/testData/diagnostics/testsWithJsStdLib/export/wrongExportedDeclaration.kt
@@ -18,3 +18,6 @@ enum class <!WRONG_EXPORTED_DECLARATION("enum class")!>EnumClass<!> { ENTRY1, EN
 
 @JsExport
 annotation class <!WRONG_EXPORTED_DECLARATION("annotation class")!>AnnotationClass<!>
+
+@JsExport
+interface <!WRONG_EXPORTED_DECLARATION("interface")!>SomeInterface<!>
