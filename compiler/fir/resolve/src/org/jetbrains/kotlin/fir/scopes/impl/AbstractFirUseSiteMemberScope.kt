e5f5adfcdf31b03f19a4bfd7512f279f3ff9a1a2
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-01-12 17:50:45 +0100

    FIR IDE: require function to be resolved to IMPLICIT_TYPES_BODY_RESOLVE for checking overrides

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/scopes/impl/AbstractFirUseSiteMemberScope.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/scopes/impl/AbstractFirUseSiteMemberScope.kt
@@ -56,7 +56,7 @@ abstract class AbstractFirUseSiteMemberScope(
         val result = mutableListOf<FirNamedFunctionSymbol>()
         val firSimpleFunction = symbol.fir
         superTypesScope.processFunctionsByName(symbol.callableId.callableName) { superSymbol ->
-            superSymbol.ensureResolved(FirResolvePhase.STATUS, session)
+            superSymbol.ensureResolved(FirResolvePhase.IMPLICIT_TYPES_BODY_RESOLVE, session)
             if (overrideChecker.isOverriddenFunction(firSimpleFunction, superSymbol.fir)) {
                 result.add(superSymbol)
             }
