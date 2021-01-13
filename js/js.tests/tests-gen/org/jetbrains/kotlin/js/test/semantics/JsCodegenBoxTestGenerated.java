9d1d8d187cbe9cd72c661eea39b31d2ffefdbd48
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-01-13 23:08:55 +0100

    Remove kt30402 since it covered completely with foldingBinaryOpsUnsigned

--- js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/semantics/JsCodegenBoxTestGenerated.java
+++ js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/semantics/JsCodegenBoxTestGenerated.java
@@ -26234,11 +26234,6 @@ public class JsCodegenBoxTestGenerated extends AbstractJsCodegenBoxTest {
             runTest("compiler/testData/codegen/box/unsignedTypes/kt25784.kt");
         }
 
-        @TestMetadata("kt30402.kt")
-        public void testKt30402() throws Exception {
-            runTest("compiler/testData/codegen/box/unsignedTypes/kt30402.kt");
-        }
-
         @TestMetadata("kt43286.kt")
         public void testKt43286() throws Exception {
             runTest("compiler/testData/codegen/box/unsignedTypes/kt43286.kt");
