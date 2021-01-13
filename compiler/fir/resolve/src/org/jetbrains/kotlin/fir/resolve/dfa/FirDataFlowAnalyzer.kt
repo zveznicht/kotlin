a6ea7d977003ccc0e38c2b007cc3742fd0228471
Denis.Zharkov <denis.zharkov@jetbrains.com>
2021-01-13 12:16:03 +0300

    FIR: Add callable references nodes to CFG
    Otherwise, they are not being considered as return expressions from lambdas

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/dfa/FirDataFlowAnalyzer.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/dfa/FirDataFlowAnalyzer.kt
@@ -1111,6 +1111,12 @@ abstract class FirDataFlowAnalyzer<FLOW : Flow>(
         graphBuilder.exitElvis().mergeIncomingFlow()
     }
 
+    // Callable reference
+
+    fun exitCallableReference(callableReferenceAccess: FirCallableReferenceAccess) {
+        graphBuilder.exitCallableReference(callableReferenceAccess).mergeIncomingFlow()
+    }
+
     // ------------------------------------------------------ Utils ------------------------------------------------------
 
     private var CFGNode<*>.flow: FLOW
