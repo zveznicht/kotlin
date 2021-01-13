b12c3d6379325a39bfa98c1b5e166297e76701b2
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-01-13 03:18:37 +0100

    Update test with byte and long unsigned values

--- compiler/testData/codegen/box/constants/foldingBinaryOpsUnsigned.kt
+++ compiler/testData/codegen/box/constants/foldingBinaryOpsUnsigned.kt
@@ -1,9 +1,17 @@
 
-val a = "CONCAT " + 0x8fffffffU
+val a = "INT " + 0x8fffffffU
+val b = "BYTE " + 0x8ffU
+val c = "LONG " + 0xffff_ffff_ffffU
 
 fun box(): String {
-    if (a != "CONCAT 2415919103") {
+    if (a != "INT 2415919103") {
         return "FAIL 0: $a"
+    }
+    if (b != "BYTE 2303") {
+        return "FAIL 1: $b"
+    }
+    if (c != "LONG 281474976710655") {
+        return "FAIL 1: $c"
     }
     return "OK"
 }
