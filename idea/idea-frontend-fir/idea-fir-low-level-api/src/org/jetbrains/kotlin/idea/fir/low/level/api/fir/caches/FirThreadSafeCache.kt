daf41f44d6fffcbc4eb8483b2459d41d20247d61
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-01-15 18:03:05 +0100

    FIR IDE: implement thread safe fir caches for IDE

---
+++ idea/idea-frontend-fir/idea-fir-low-level-api/src/org/jetbrains/kotlin/idea/fir/low/level/api/fir/caches/FirThreadSafeCache.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.idea.fir.low.level.api.fir.caches
+
+import org.jetbrains.kotlin.fir.caches.FirCache
+import java.util.concurrent.ConcurrentHashMap
+
+internal class FirThreadSafeCache<KEY : Any, VALUE>(private val createValue: (KEY) -> VALUE) : FirCache<KEY, VALUE> {
+    private val map = ConcurrentHashMap<KEY, Any>()
+
+    override fun getValue(key: KEY): VALUE =
+        map.computeIfAbsentWithNullableValue(key, createValue)
+}
